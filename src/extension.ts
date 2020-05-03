04e5d8e1883a06ab2ade0dfe723de490fec8046e
Dima <dima@codota.com>
2020-05-03 10:54:03 +0300

    typo

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ const CHAR_LIMIT = 100000;
 const MAX_NUM_RESULTS = 5;
 const DEFAULT_DETAIL = "TabNine";
 
-const COMPLEXATION_IMPORTS = 'tabnine-completion-imports';
+const COMPLETION_IMPORTS = 'tabnine-completion-imports';
 export function activate(context: vscode.ExtensionContext) {
 
   const command = 'TabNine::config';
@@ -31,7 +31,7 @@ export function activate(context: vscode.ExtensionContext) {
           let [firstCommand] = importCommands;
           await vscode.workspace.applyEdit(firstCommand.edit);
           await vscode.commands.executeCommand(firstCommand.command.command, firstCommand.command.arguments);
-          await vscode.commands.executeCommand(COMPLEXATION_IMPORTS, {completion} );
+          await vscode.commands.executeCommand(COMPLETION_IMPORTS, {completion} );
         }
       } catch (error) {
         console.log(error);
@@ -39,7 +39,7 @@ export function activate(context: vscode.ExtensionContext) {
     }, 400);
   }
   
-  context.subscriptions.push(vscode.commands.registerTextEditorCommand(COMPLEXATION_IMPORTS, importsCommand));
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand(COMPLETION_IMPORTS, importsCommand));
 
   context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
 
@@ -169,7 +169,7 @@ export function activate(context: vscode.ExtensionContext) {
     item.insertText = new vscode.SnippetString(escapeTabStopSign(args.entry.new_prefix));
     item.command = {
       arguments: [{ completion: args.entry.new_prefix}],
-      command: COMPLEXATION_IMPORTS,
+      command: COMPLETION_IMPORTS,
       title: "accept completion",
     };
     if (args.entry.new_suffix) {
