0b65722a71a0a9a05d784ce1575f50a126e3f17f
dimacodota <60742964+dimacodota@users.noreply.github.com>
2020-08-10 17:23:15 +0300

    1. completely removed the “internet connection error” message. (#131)
    2. limited the “You had registered to TabNine Professional but didn’t enable the Deep Cloud” message to be shown only once.
    3. all the “old” user notifications moved back to completion details window.

--- src/notificationsHandler.ts
+++ src/notificationsHandler.ts
@@ -1,16 +1,13 @@
 import { API_VERSION, TabNine, StateType, StatePayload } from "./TabNine";
-import { window, commands } from "vscode";
+import { window, commands, ExtensionContext } from "vscode";
 import { EOL } from "os";
 import { CONFIG_COMMAND } from "./commandsHandler";
+import { once } from "./utils";
 const memoize = require('lodash.memoize');
 
-const CONNECTION_ISSUE = "TabNine deep cloud requires an active internet connection. Please check your connection. You can still work locally with TabNine deep local";
-const CONNECTION_ISSUE_WARNING = "TabNine deep cloud requires an active internet connection, please check your internet connection and reconnect again";
 const CLOUD_CAPABLE_NOT_ENABLED = "You had registered to TabNine Professional but didn't enable the Deep Cloud";
 const ENABLED_CLOUD_ACTION = "Enable Deep Cloud";
-
-const FIRST_NOTIFICATION_DELAY = 10000;
-
+const ENABLED_CLOUD_NOTIFICATION_KEY = "tabnine.hide.enable-cloud.notification"
 
 export const handleInfoMessage = memoize((tabNine:TabNine, message: string, onClick = (action: string) => {},...args: string[]) => {
   tabNine.setState({ [StatePayload.message]: {message_type: StateType.info, message}});
@@ -33,16 +30,15 @@ export function handleUserMessage(tabNine: TabNine, { user_message } ) {
 }
 
 
-export async function handleStartUpNotification(tabNine: TabNine) {
+export async function handleStartUpNotification(tabNine: TabNine,context: ExtensionContext) {
   let {
     cloud_enabled,
-    local_enabled,
-    is_cloud_capable,
-    is_authenticated
+    is_cloud_capable
   } = await tabNine.request(API_VERSION, { State: {} });
 
-  handleConnectivity(cloud_enabled, local_enabled, is_authenticated, tabNine);
-  handleCloudEnabling(is_cloud_capable, cloud_enabled, tabNine);
+  once(ENABLED_CLOUD_NOTIFICATION_KEY, context).then(() => {
+    handleCloudEnabling(is_cloud_capable, cloud_enabled, tabNine);
+  })
 }
 
 function handleCloudEnabling(is_cloud_capable: boolean, cloud_enabled: boolean, tabNine: TabNine) {
@@ -50,25 +46,6 @@ function handleCloudEnabling(is_cloud_capable: boolean, cloud_enabled: boolean, 
     handleInfoMessage(tabNine, CLOUD_CAPABLE_NOT_ENABLED, onEnableCloudAction, ENABLED_CLOUD_ACTION);
   }
 };
-
-function handleConnectivity(cloud_enabled: boolean, local_enabled: boolean, is_authenticated: boolean, tabNine: TabNine) {
-  if (!is_authenticated) {
-    setTimeout(async () => {
-      let {
-        cloud_enabled,
-        local_enabled,
-        is_authenticated, } = await tabNine.request(API_VERSION, { State: {} });
-
-      if (cloud_enabled && !is_authenticated) {
-        if (!local_enabled){
-          handleErrorMessage(tabNine, CONNECTION_ISSUE, onEnableCloudAction, ENABLED_CLOUD_ACTION);
-        } else {
-          handleWarningMessage(tabNine, CONNECTION_ISSUE_WARNING);
-        }
-      }
-    }, FIRST_NOTIFICATION_DELAY);
-  }
-}
 
 export function onEnableCloudAction(action: string): any {
   if (action === ENABLED_CLOUD_ACTION) {
