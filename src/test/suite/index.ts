c41710339ff678f562cb768fb2d04f9ab7d9c566
Dima <dima@codota.com>
2020-09-29 16:07:17 +0300

    mocha-teamcity-reporter added

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -7,6 +7,7 @@ export function run(): Promise<void> {
 	const mocha = new Mocha({
         ui: 'tdd',
         color: true,
+        reporter: "mocha-teamcity-reporter"
 	});
 
 	const testsRoot = path.resolve(__dirname, '..');
