f77949f2ef7b5ccf3299ca49e975f1f470192276
Dima <dima@codota.com>
2020-09-30 17:22:48 +0300

    reporter added

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -7,6 +7,8 @@ export function run(): Promise<void> {
 	const mocha = new Mocha({
         ui: 'tdd',
         color: true,
+        reporter: "mocha-teamcity-reporter"
+
     });
     //reporter: "mocha-teamcity-reporter"
 
