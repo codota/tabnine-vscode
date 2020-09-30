19725ca95a6b8db057de66602be7f2a2f6caca1e
Dima <dima@codota.com>
2020-09-30 12:37:46 +0300

    install libX11

--- teamcity-tests.sh
+++ teamcity-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-sudo apt-get install -y libX11-xcb
+apt-get install -y libX11-xcb
 
 export DISPLAY=':99.0'
 /usr/bin/Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
