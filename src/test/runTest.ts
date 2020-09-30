eba7b4aa7a830d5815a25649e6a857d9a0ee915e
Dima <dima@codota.com>
2020-09-30 13:42:37 +0300

    fix test run  exit code

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -13,7 +13,8 @@ async function main() {
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+        let exitCode = await runTests({ extensionDevelopmentPath, extensionTestsPath });
+        process.exit(exitCode);
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
