9ba913a317fb215f66833594fb8d3ea1aafaa894
dimacodota <60742964+dimacodota@users.noreply.github.com>
2020-11-15 10:58:28 +0200

    notifications for alpha only (#203)
    * notifications for alpha only
    
    * fix test- capability init added
    
    Former-commit-id: 9105cfa92258f1df4de03720ba421840d4e2b3d8

--- src/binary/mockedRunProcess.ts
+++ src/binary/mockedRunProcess.ts
@@ -3,6 +3,8 @@ import { ReadLine } from "readline";
 import * as stream from "stream";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import * as TypeMoq from "typemoq";
+import { Capability } from "../capabilities";
+import { CapabilitiesResponse } from "./requests/requests";
 import { BinaryProcessRun } from "./runProcess";
 
 export const spawnedProcessMock: TypeMoq.IMock<child_process.ChildProcess> = TypeMoq.Mock.ofType<
@@ -20,16 +22,42 @@ export const stdoutMock: TypeMoq.IMock<stream.Readable> = TypeMoq.Mock.ofType<
 
 export default function mockedRunProcess(): BinaryProcessRun {
   spawnedProcessMock.setup((x) => x.killed).returns(() => false);
-  readLineMock
-    .setup((x) => x.once("line", TypeMoq.It.isAny()))
-    .callback((x, callback: (line: string) => void) => {
-      callback("null");
-    });
   spawnedProcessMock.setup((x) => x.stdin).returns(() => stdinMock.object);
   spawnedProcessMock.setup((x) => x.stdout).returns(() => stdoutMock.object);
-
+  setCapabilities({
+    enabled_features: [Capability.ALPHA_CAPABILITY]
+  })
   return {
     proc: spawnedProcessMock.object,
     readLine: readLineMock.object,
   };
 }
+function setCapabilities(
+  capabilities: CapabilitiesResponse
+): void {
+  let requestHappened = false;
+  stdinMock.setup((x) =>
+    x.write(
+      TypeMoq.It.is<string>((request) => {
+        const capabilitiesRequest = JSON.parse(request) as { request: { Features: Record<string, unknown> } };
+        if (capabilitiesRequest?.request?.Features) {
+          requestHappened = true;
+          return true;
+        }
+
+        return false;
+      }),
+      "utf8"
+    )
+  );
+  readLineMock
+    .setup((x) => x.once("line", TypeMoq.It.isAny()))
+    .callback((x, callback: (line: string) => void) => {
+      if (!requestHappened) {
+        callback("null");
+      } else {
+        callback(JSON.stringify(capabilities));
+      }
+    });
+}
+
