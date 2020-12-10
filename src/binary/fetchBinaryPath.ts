48b69a8da817368b465474f9824431969f369cd3
amircodota <amirtu@codota.com>
2020-12-10 15:36:35 +0200

    use binaries/.active file to select the version to run if such a file exists (#261)
    * use binaries/.active file to select the version to run if such a file exists
    
    * no need to write .active during binary download - this file is not under vcs anyways
    
    * error handling
    
    Former-commit-id: ae2c89aad5fd7f1df39843a98579b484ddea713b [formerly 9dc026af11834eaa1b8aebe3ddd9ceefa8e08f3e]
    Former-commit-id: 8881ffb35133b3e04d1021de5b65d49daabef191

--- src/binary/fetchBinaryPath.ts
+++ src/binary/fetchBinaryPath.ts
@@ -1,9 +1,25 @@
 import * as fs from "fs";
+import * as path from 'path';
 import { getRootPath, versionPath } from "./paths";
 import sortBySemver from "../semverUtils";
 
 export default function fetchBinaryPath(): string {
-  const versions = sortBySemver(fs.readdirSync(getRootPath())).map(versionPath);
+  const rootPath = getRootPath();
+
+  try {
+    const activePath = path.join(rootPath, '.active');
+    if (fs.existsSync(activePath)) {
+      const activeVersion = fs.readFileSync(activePath, 'utf-8').trim();
+      const activeVersionPath = versionPath(activeVersion);
+      if (fs.existsSync(activeVersionPath)) {
+          return activeVersionPath;
+      }
+    }
+  } catch(e) {
+    console.error("Error handling .active file. Falling back to semver sorting", e);
+  }
+
+  const versions = sortBySemver(fs.readdirSync(rootPath)).map(versionPath);
   const selectedVersion = versions.find(fs.existsSync);
 
   if (!selectedVersion) {
