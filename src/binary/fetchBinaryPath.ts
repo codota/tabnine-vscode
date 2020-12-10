8abee2e218952bfdb67dac5dc829a7d1078dbae6
Amir Tuval <amirtu@codota.com>
2020-12-10 11:36:40 +0200

    error handling

--- src/binary/fetchBinaryPath.ts
+++ src/binary/fetchBinaryPath.ts
@@ -5,13 +5,18 @@ import sortBySemver from "../semverUtils";
 
 export default function fetchBinaryPath(): string {
   const rootPath = getRootPath();
-  const activePath = path.join(rootPath, '.active');
-  if (fs.existsSync(activePath)) {
-    const activeVersion = fs.readFileSync(activePath, 'utf-8').trim();
-    const activeVersionPath = versionPath(activeVersion);
-    if (fs.existsSync(activeVersionPath)) {
-        return activeVersionPath;
+
+  try {
+    const activePath = path.join(rootPath, '.active');
+    if (fs.existsSync(activePath)) {
+      const activeVersion = fs.readFileSync(activePath, 'utf-8').trim();
+      const activeVersionPath = versionPath(activeVersion);
+      if (fs.existsSync(activeVersionPath)) {
+          return activeVersionPath;
+      }
     }
+  } catch(e) {
+    console.error("Error handling .active file. Falling back to semver sorting", e);
   }
 
   const versions = sortBySemver(fs.readdirSync(rootPath)).map(versionPath);
