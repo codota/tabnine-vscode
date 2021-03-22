7c4659c86848383c552d9abb10ff96c20bf9619b
Dima <dima@codota.com>
2021-03-22 16:53:45 +0200

    test again

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,12 +1,12 @@
 import * as path from "path";
 import * as Mocha from "mocha";
 import * as glob from "glob";
-// import * as ncp from "ncp";
-// import * as rimraf from "rimraf";
-// import { promisify } from "util";
+import * as ncp from "ncp";
+import * as rimraf from "rimraf";
+import { promisify } from "util";
 
-// const promisifyNcp = promisify(ncp);
-// const promisifyRimraf = promisify(rimraf);
+const promisifyNcp = promisify(ncp);
+const promisifyRimraf = promisify(rimraf);
 
 // This is required to run the tests. Do not change to default export.
 // eslint-disable-next-line import/prefer-default-export
@@ -30,21 +30,32 @@ export function run(): Promise<void> {
       files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
       // await copyTestBinaries(fixtureBinary, targetBinary);
 
-      // const fixtureBinary = path.resolve(__dirname, "./fixture/binaries");
-      // const targetBinary = path.resolve(__dirname,  "binaries");
+      const fixtureBinary = path.resolve(
+        __dirname,
+        "..",
+        "fixture",
+        "binaries"
+      );
+      const targetBinary = path.resolve(
+        __dirname,
+        "..",
+        "..",
+        "..",
+        "binaries"
+      );
       try {
         // Run the mocha test
-        // void copyTestBinaries(path.resolve(__dirname, "../../.."),).then(() => {
-        mocha.run((failures) => {
-          // void clearTestBinaries(path.resolve(__dirname, "../../..")).then(() => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-          // });
+        void copyTestBinaries(fixtureBinary, targetBinary).then(() => {
+          mocha.run((failures) => {
+            void clearTestBinaries(targetBinary).then(() => {
+              if (failures > 0) {
+                e(new Error(`${failures} tests failed.`));
+              } else {
+                c();
+              }
+            });
+          });
         });
-        // })
       } catch (error) {
         console.error(error);
         e(error);
@@ -53,15 +64,15 @@ export function run(): Promise<void> {
   });
 }
 
-// function copyTestBinaries(
-//   fixtureBinary: string,
-//   targetBinary: string
-// ): Promise<void> {
-//   console.log("copy test binaries");
-//   return promisifyNcp(fixtureBinary, targetBinary);
-// }
+function copyTestBinaries(
+  fixtureBinary: string,
+  targetBinary: string
+): Promise<void> {
+  console.log("copy test binaries");
+  return promisifyNcp(fixtureBinary, targetBinary);
+}
 
-// function clearTestBinaries(targetBinary: string): Promise<void> {
-//   console.log("clear test binaries");
-//   return promisifyRimraf(targetBinary);
-// }
+function clearTestBinaries(targetBinary: string): Promise<void> {
+  console.log("clear test binaries");
+  return promisifyRimraf(targetBinary);
+}
