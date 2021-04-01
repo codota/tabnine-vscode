c49f81af74f07390232e5fd3aa3ac3bc65425a0e
Dima <60742964+dimacodota@users.noreply.github.com>
2021-04-01 13:05:40 +0300

    handle published version validation (#448)
    * handle piblished version validation
    
    * assertValidVersion

--- src/binary/binaryFetcher/existingVersionHandler.ts
+++ src/binary/binaryFetcher/existingVersionHandler.ts
@@ -8,7 +8,7 @@ export default async function handleExistingVersion(): Promise<string | null> {
   try {
     const versionPaths = await fs.readdir(getRootPath());
     const versions = sortBySemver(versionPaths).map(versionPath);
-    return asyncFind(versions, isValidBinary);
+    return await asyncFind(versions, isValidBinary);
   } catch (e) {
     console.error(
       "Error handling existing version. Falling back to downloading",
