f70193e0f8101830a1fdbaf35772326e5ac23044
Amir Bilu <amirbilu@codota.com>
2021-07-07 18:05:24 +0300

    wip
    Former-commit-id: a3b10efa622e2a1b68b89b9da829d4510e708221 [formerly 0807f8454a25ac32321a95c917626bde34b39712] [formerly 849615fecf2ce1080d175fbc42715740eba3d85a [formerly a14a5e752a14b56b6e47c20121338ba7c61fb040]]
    Former-commit-id: 1c13bea0d076800cedb0535de968f98c9579a536 [formerly 8420bd81a7db37515dee4921e62855e9de3e18e3]
    Former-commit-id: 29d34857c73fb0ede242323172aeff52a1b4b130

--- src/openHub.ts
+++ src/openHub.ts
@@ -1,7 +1,29 @@
 import { Uri, ViewColumn, WebviewPanel, window } from "vscode";
+import { get } from "http";
 import * as path from "path";
 import { IS_OSX } from "./globals/consts";
 import { fireEvent } from "./binary/requests/requests";
+
+const layout = (content: string) => `
+<!DOCTYPE html>
+<html lang="en" style="margin: 0; padding: 0; min-width: 100%; min-height: 100%">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tabnine Hub</title>
+    </head>
+    <body style="margin: 0; padding: 0; min-width: 100%; min-height: 100%">
+        ${content}
+    </body>
+</html>`;
+
+function waitForHub(uri: Uri): Promise<void> {
+  return new Promise((resolve, reject) => {
+    get(uri.toString(), (res) => {
+      resolve();
+    });
+  });
+}
 
 export default function openHub(uri: Uri): WebviewPanel {
   const panel = window.createWebviewPanel(
@@ -28,45 +50,65 @@ export default function openHub(uri: Uri): WebviewPanel {
       name: "hub-view-closed",
     });
   });
+
   panel.iconPath = Uri.file(path.resolve(__dirname, "..", "small_logo.png"));
-  panel.webview.html = `
-        <!DOCTYPE html>
-        <html lang="en" style="margin: 0; padding: 0; min-width: 100%; min-height: 100%">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Tabnine Hub</title>
-            </head>
-            <body style="margin: 0; padding: 0; min-width: 100%; min-height: 100%">
-            <iframe src=${uri.toString()} id="config" frameborder="0" style="display: block; margin: 0; padding: 0; position: absolute; min-width: 100%; min-height: 100%; visibility: visible;"></iframe>
-                <script>
-                    window.onfocus = config.onload = function() {
-                        setTimeout(function() {
-                            document.getElementById("config").contentWindow.focus();
-                        }, 100);
-                    };
-                    window.addEventListener("message", (e) => {
-                      let data = e.data;
-                      switch (data.type) {
-                        case "keydown": {
-                          if (${IS_OSX}) {
-                            window.dispatchEvent(new KeyboardEvent('keydown',data.event));
-                          }
-                          break;
-                        }
-                        case "link-click": {
-                          let tempRef = document.createElement("a");
-                          tempRef.setAttribute("href", data.href);
-                          config.appendChild(tempRef);
-                          tempRef.click();
-                          tempRef.parentNode.removeChild(tempRef);
-                          break;
-                        }
-                      }
-                  }, false);
-                  </script>
-            </body>
-        </html>`;
+
+  panel.webview.html = layout(`
+    <div
+      id="loading"
+      frameborder="0"
+      style="
+        display: block;
+        margin: 0;
+        padding: 0;
+        position: absolute;
+        min-width: 100%;
+        min-height: 100%;
+        visibility: visible;
+        background: rgb(37, 37, 38);
+        color: white;
+        z-index: 2;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      "
+    >
+      Loading ...
+    </div>
+   `);
+
+  waitForHub(uri).then(() => {
+    panel.webview.html = layout(`
+    <iframe src=${uri.toString()} id="config" frameborder="0" style="display: block; margin: 0; padding: 0; position: absolute; min-width: 100%; min-height: 100%; visibility: visible;"></iframe>
+    <script>
+        window.onfocus = config.onload = function() {
+            setTimeout(function() {
+                document.getElementById("config").contentWindow.focus();
+            }, 100);
+        };
+        window.addEventListener("message", (e) => {
+          let data = e.data;
+          switch (data.type) {
+            case "keydown": {
+              if (${IS_OSX}) {
+                window.dispatchEvent(new KeyboardEvent('keydown',data.event));
+              }
+              break;
+            }
+            case "link-click": {
+              let tempRef = document.createElement("a");
+              tempRef.setAttribute("href", data.href);
+              config.appendChild(tempRef);
+              tempRef.click();
+              tempRef.parentNode.removeChild(tempRef);
+              break;
+            }
+          }
+      }, false);
+
+      </script>
+    `);
+  });
 
   return panel;
 }
