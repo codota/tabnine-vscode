6053e32c7a03c16fc84412cb7814f1d5ad638c55
Amir Bilu <amirbilu@codota.com>
2021-07-15 13:04:42 +0300

    watch for token changes
    Former-commit-id: 155c5251fda3046697a8c15a415ff6a1848cc756 [formerly 043ed7d8d9ebc98ed4a363ebb50f9005d20d8d45] [formerly 43cd4de40203efebfada11ab71bcc97eff122ce6 [formerly 6850c8102121bdc941dfb09394b0e15a0a1e587c]]
    Former-commit-id: e13208b3b13bb203d6ebe8627ed9a0bb2353c64a [formerly 8b7b19efe767dbc7ad12523a5d36ea78c2bf72b6]
    Former-commit-id: cf9a1f94d71f3604a91af28e9406a6cea19bccf4

---
+++ src/gitpod/consts.ts
@@ -0,0 +1,22 @@
+import * as path from "path";
+import * as os from "os";
+
+export const TABNINE_CONFIG_DIR = path.join(os.homedir(), ".config", "TabNine");
+
+export const TABNINE_TOKEN_FILE_NAME = "tabnine.token";
+
+export const TABNINE_TOKEN_FILE_PATH = path.join(
+  TABNINE_CONFIG_DIR,
+  TABNINE_TOKEN_FILE_NAME
+);
+
+export const TABNINE_CONFIG_FILE_NAME = "tabnine_config.json";
+
+export const TABNINE_CONFIG_FILE_PATH = path.join(
+  TABNINE_CONFIG_DIR,
+  TABNINE_CONFIG_FILE_NAME
+);
+
+export const TABNINE_TOKEN_ENV_VAR = "TABNINE_TOKEN";
+
+export const TABNINE_CONFIG_ENV_VAR = "TABNINE_CONFIG";
--- src/gitpod/state.ts
+++ src/gitpod/state.ts
@@ -1,36 +1,15 @@
-import * as path from "path";
-import * as os from "os";
 import { promises as fsPromises, watch } from "fs";
 import { setEnvVar } from "./cli";
 import { fromBase64, toBase64 } from "../utils/utils";
-
-const TABNINE_CONFIG_DIR = path.join(os.homedir(), ".config", "TabNine");
-
-const TABNINE_TOKEN_FILE_NAME = "tabnine.token";
-
-const TABNINE_TOKEN_FILE_PATH = path.join(
-  TABNINE_CONFIG_DIR,
-  TABNINE_TOKEN_FILE_NAME
-);
-
-const TABNINE_CONFIG_FILE_NAME = "tabnine_config.json";
-
-const TABNINE_CONFIG_FILE_PATH = path.join(
-  TABNINE_CONFIG_DIR,
-  TABNINE_CONFIG_FILE_NAME
-);
-
-const TABNINE_TOKEN_ENV_VAR = "TABNINE_TOKEN";
-
-const TABNINE_CONFIG_ENV_VAR = "TABNINE_CONFIG";
+import * as consts from "./consts";
 
 export async function loadStateFromGitpodEnvVar(): Promise<void> {
-  const tabnineToken = process.env[TABNINE_TOKEN_ENV_VAR];
-  const tabnineConfig = process.env[TABNINE_CONFIG_ENV_VAR];
+  const tabnineToken = process.env[consts.TABNINE_TOKEN_ENV_VAR];
+  const tabnineConfig = process.env[consts.TABNINE_CONFIG_ENV_VAR];
 
   if (tabnineToken) {
     await fsPromises
-      .writeFile(TABNINE_TOKEN_FILE_PATH, fromBase64(tabnineToken))
+      .writeFile(consts.TABNINE_TOKEN_FILE_PATH, fromBase64(tabnineToken))
       .catch((e) => {
         console.error("Error occurred while trying to load Tabnine token", e);
       });
@@ -38,37 +17,42 @@ export async function loadStateFromGitpodEnvVar(): Promise<void> {
 
   if (tabnineConfig)
     await fsPromises
-      .writeFile(TABNINE_TOKEN_FILE_PATH, fromBase64(tabnineConfig))
+      .writeFile(consts.TABNINE_TOKEN_FILE_PATH, fromBase64(tabnineConfig))
       .catch((e) => {
         console.error("Error occurred while trying to load Tabnine config", e);
       });
 }
 
 export function persistStateToGitpodEnvVar(): void {
-  watch(TABNINE_CONFIG_DIR, (event, filename) => {
-    if (filename === TABNINE_TOKEN_FILE_NAME)
-      void fsPromises
-        .readFile(TABNINE_TOKEN_FILE_PATH, "utf8")
-        .then((tabnineToken) =>
-          setEnvVar(TABNINE_TOKEN_ENV_VAR, toBase64(tabnineToken))
-        )
-        .catch((e) => {
-          console.error(
-            "Error occurred while trying to persist Tabnine token",
-            e
-          );
-        });
-    if (filename === TABNINE_CONFIG_FILE_NAME)
-      void fsPromises
-        .readFile(TABNINE_CONFIG_FILE_PATH, "utf8")
-        .then((tabnineConfig) =>
-          setEnvVar(TABNINE_CONFIG_ENV_VAR, toBase64(tabnineConfig))
-        )
-        .catch((e) => {
-          console.error(
-            "Error occurred while trying to persist Tabnine config",
-            e
-          );
-        });
+  watch(consts.TABNINE_CONFIG_DIR, (event, filename) => {
+    switch (filename) {
+      case consts.TABNINE_TOKEN_FILE_NAME:
+        void fsPromises
+          .readFile(consts.TABNINE_TOKEN_FILE_PATH, "utf8")
+          .then((tabnineToken) =>
+            setEnvVar(consts.TABNINE_TOKEN_ENV_VAR, toBase64(tabnineToken))
+          )
+          .catch((e) => {
+            console.error(
+              "Error occurred while trying to persist Tabnine token",
+              e
+            );
+          });
+        break;
+      case consts.TABNINE_CONFIG_FILE_NAME:
+        void fsPromises
+          .readFile(consts.TABNINE_CONFIG_FILE_PATH, "utf8")
+          .then((tabnineConfig) =>
+            setEnvVar(consts.TABNINE_CONFIG_ENV_VAR, toBase64(tabnineConfig))
+          )
+          .catch((e) => {
+            console.error(
+              "Error occurred while trying to persist Tabnine config",
+              e
+            );
+          });
+        break;
+      default:
+    }
   });
 }
