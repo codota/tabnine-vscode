449d06fa43a73cc94adae6f791cecc88f98c3814
Dima <dima@codota.com>
2021-07-24 19:50:20 +0300

    check for new line when calculating the new position

--- src/inlineSuggestions/textListener.ts
+++ src/inlineSuggestions/textListener.ts
@@ -1,4 +1,5 @@
 import { Position, TextDocumentChangeEvent } from "vscode";
+import { EOL } from "os";
 import {
   getCurrentSuggestion,
   setSuggestionsState,
@@ -26,8 +27,13 @@ export default async function textListener(
 }
 
 export function getCurrentPosition(data: TextDocumentChangeEvent): Position {
+  const lineDelta = getNewLinesCount(data.contentChanges[0].text);
+  const characterDelta = data.contentChanges[0].text.length;
   return data.contentChanges[0].range.start.translate(
-    undefined,
-    data.contentChanges[0].text.length
+    lineDelta,
+    characterDelta
   );
+}
+function getNewLinesCount(text: string) {
+  return text.split(EOL).length - 1;
 }
