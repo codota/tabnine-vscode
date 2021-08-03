4792d370e423efba3bb33c35693fbfc8c2a5b2a2
Amir Bilu <amirbilu@codota.com>
2021-08-03 17:52:31 +0300

    sync keys

--- src/extension.ts
+++ src/extension.ts
@@ -40,15 +40,13 @@ import getSuggestionMode, {
   SuggestionsMode,
 } from "./capabilities/getSuggestionMode";
 import isGitpod from "./gitpod/isGitpod";
-import { loadStateFromGitpod, persistStateToGitpod } from "./gitpod/state";
+import setupGitpodState from "./gitpod/setupGitpodState";
 
 export async function activate(
   context: vscode.ExtensionContext
 ): Promise<void> {
-  if (isGitpod) {
-    await loadStateFromGitpod(context);
-    void persistStateToGitpod(context);
-  }
+  if (isGitpod) await setupGitpodState(context);
+
   void initStartup(context);
   handleSelection(context);
   handleUninstall(() => uponUninstall(context));
