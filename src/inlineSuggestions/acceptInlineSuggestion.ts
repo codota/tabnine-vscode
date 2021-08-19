cf470a1f0376c499fbd8631ea0503598f8ff322a
Eran Yahav <yahave@gmail.com>
2021-08-19 14:28:54 +0300

    hfix snippet indentation

--- src/inlineSuggestions/acceptInlineSuggestion.ts
+++ src/inlineSuggestions/acceptInlineSuggestion.ts
@@ -1,3 +1,4 @@
+import { EOL } from "os";
 import { commands, Position, Range, SnippetString, TextEditor } from "vscode";
 import { ResultEntry } from "../binary/requests/requests";
 import { CompletionArguments } from "../CompletionArguments";
@@ -18,12 +19,32 @@ export default async function acceptInlineSuggestion(
   const prefix = getCurrentPrefix();
   const allSuggestions = getAllSuggestions();
   if (currentSuggestion && currentTextPosition && allSuggestions) {
-    const range = getSuggestionRange(
-      currentTextPosition,
-      prefix,
-      currentSuggestion.old_suffix
+    
+    
+
+    let rangeStartTranslate = -currentTextPosition.character;
+    let firstLineIndent = currentTextPosition.character;
+
+    if (getLinesCount(currentSuggestion.new_prefix) <= 1) { 
+      rangeStartTranslate = -prefix.trim().length;
+      firstLineIndent = 0;
+    } 
+
+    let indentSpaces = ' '.repeat(firstLineIndent);
+
+    const insertText = constructInsertSnippet(indentSpaces, currentSuggestion);
+
+    const range = new Range(
+      currentTextPosition.translate(undefined, rangeStartTranslate),
+      getEndPosition(insertText.value,currentTextPosition),
     );
-    const insertText = constructInsertSnippet(currentSuggestion);
+
+    // const range = getSuggestionRange(
+    //   currentTextPosition,
+    //   prefix,
+    //   insertText.value,
+    //   currentSuggestion.old_suffix
+    // );
 
     const completion: CompletionArguments = {
       currentCompletion: currentSuggestion.new_prefix,
@@ -38,8 +59,8 @@ export default async function acceptInlineSuggestion(
   }
 }
 
-function constructInsertSnippet({ new_prefix, new_suffix }: ResultEntry) {
-  const insertText = new SnippetString(escapeTabStopSign(new_prefix));
+function constructInsertSnippet(indentSpaces : string, { new_prefix, new_suffix }: ResultEntry) {
+  const insertText = new SnippetString(escapeTabStopSign(indentSpaces + new_prefix));
 
   if (new_suffix) {
     insertText.appendTabstop(0).appendText(escapeTabStopSign(new_suffix));
@@ -47,13 +68,29 @@ function constructInsertSnippet({ new_prefix, new_suffix }: ResultEntry) {
   return insertText;
 }
 
-function getSuggestionRange(
-  currentTextPosition: Position,
-  prefix: string,
-  oldPrefix: string
-) {
-  return new Range(
-    currentTextPosition.translate(undefined, -prefix.trim().length),
-    currentTextPosition.translate(undefined, oldPrefix.trim().length)
-  );
+// function getSuggestionRange(
+//   currentTextPosition: Position,
+//   prefix: string,
+//   insertText : string,
+//   oldPrefix: string
+// ) {
+//   return new Range(
+//     currentTextPosition.translate(undefined, -4), //-prefix.length),
+//     getEndPosition(insertText,currentTextPosition),
+//   );
+// }
+
+export function getEndPosition(
+  text: string, start: Position
+): Position {
+  const lineDelta = getLinesCount(text);
+  const lines = text.split(EOL);
+  const lastLineLength = lines[lines.length - 1].length;
+  
+  return start.translate(lineDelta, lastLineLength);
 }
+
+function getLinesCount(text: string) {
+  return text.split(EOL).length - 1;
+}
+
