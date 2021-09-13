b9e1698178260e990e0f8da4d8ea55c0ff381274
Dima Abramovich <60742964+dimacodota@users.noreply.github.com>
2021-09-13 18:30:52 +0300

    suport  untrustedWorkspaces + enable tree view for all (#635)
    Former-commit-id: cb4070112a62a2b86efcf38b2de515b3ab27fde3 [formerly e41aa0329cfd0224147dbdc42d369c3134c5e691] [formerly bd1640d04ff9c9a8eab8732a72a9bff0beff0d1e [formerly b125482407e22356d5322bed98c41fd1627b34e9]]
    Former-commit-id: f339d587e420dbed0f0415f233f00413a3311511 [formerly 14d062637876503f5ac6bb5cfb2d220f8abc0b00]
    Former-commit-id: a7c5c3702f4a9961d2fa44c17ab90e4243395bfb

--- src/treeView/registerTreeView.ts
+++ src/treeView/registerTreeView.ts
@@ -1,6 +1,5 @@
 import { commands, ExtensionContext, window } from "vscode";
 import { getHubStructure } from "../binary/requests/hubStructure";
-import { Capability, isCapabilityEnabled } from "../capabilities/capabilities";
 import { TABNINE_TREE_NAVIGATION_COMMAND } from "../globals/consts";
 import navigate from "./navigate";
 import TabnineTreeProvider from "./TabnineTreeProvider";
@@ -8,29 +7,26 @@ import TabnineTreeProvider from "./TabnineTreeProvider";
 export default async function registerTreeView(
   context: ExtensionContext
 ): Promise<void> {
-  const isNavigationEnabled =
-    isCapabilityEnabled(Capability.LEFT_TREE_VIEW) ||
-    isCapabilityEnabled(Capability.ALPHA_CAPABILITY);
+  try {
+    const structure = await getHubStructure();
 
-  if (!isNavigationEnabled) {
-    return;
-  }
-  const structure = await getHubStructure();
-
-  if (structure) {
-    void commands.executeCommand(
-      "setContext",
-      "tabnine.tabnine-navigation-ready",
-      true
-    );
-    context.subscriptions.push(
-      window.registerTreeDataProvider(
-        "tabnine-home",
-        new TabnineTreeProvider()
-      ),
-      commands.registerCommand(TABNINE_TREE_NAVIGATION_COMMAND, (view) => {
-        void navigate(view);
-      })
-    );
+    if (structure) {
+      void commands.executeCommand(
+        "setContext",
+        "tabnine.tabnine-navigation-ready",
+        true
+      );
+      context.subscriptions.push(
+        window.registerTreeDataProvider(
+          "tabnine-home",
+          new TabnineTreeProvider()
+        ),
+        commands.registerCommand(TABNINE_TREE_NAVIGATION_COMMAND, (view) => {
+          void navigate(view);
+        })
+      );
+    }
+  } catch (e) {
+    console.error("Error in registerTreeView", e);
   }
 }
