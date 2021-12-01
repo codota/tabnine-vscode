6a39f58f72422025200e9b8d858c7ea04bcd5d4a
yonip23 <yoni@tabnine.com>
2021-12-01 17:44:58 +0200

    rollback snippetstring

--- src/provideInlineCompletionItems.ts
+++ src/provideInlineCompletionItems.ts
@@ -3,7 +3,6 @@ import { AutocompleteResult, ResultEntry } from "./binary/requests/requests";
 import { completionIsAllowed } from "./provideCompletionItems";
 import runCompletion from "./runCompletion";
 import { COMPLETION_IMPORTS } from "./selectionHandler";
-import { escapeTabStopSign } from "./utils/utils";
 
 const INLINE_REQUEST_TIMEOUT = 3000;
 
@@ -24,18 +23,14 @@ export default async function provideInlineCompletionItems(
       isEmptyLine ? INLINE_REQUEST_TIMEOUT : undefined
     );
 
-    const completions = response?.results.map((result) => {
-      const snippet = new vscode.SnippetString(
-        escapeTabStopSign(result.new_prefix)
-      );
-      const { value } = snippet as { value: string };
-
-      return new vscode.InlineCompletionItem(
-        value,
-        calculateRange(position, response, result),
-        getAutoImportCommand(result, response, position)
-      );
-    });
+    const completions = response?.results.map(
+      (result) =>
+        new vscode.InlineCompletionItem(
+          result.new_prefix,
+          calculateRange(position, response, result),
+          getAutoImportCommand(result, response, position)
+        )
+    );
 
     return new vscode.InlineCompletionList(completions || []);
   } catch (e) {
