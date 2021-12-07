03b6f9ba9052e94d9b536a6d65bcbfdf68938914
Dima <dima@codota.com>
2021-12-07 11:10:00 +0200

    prettier

--- src/globals/versions.ts
+++ src/globals/versions.ts
@@ -1,11 +1,10 @@
 import * as semver from "semver";
 import tabnineExtensionProperties from "./tabnineExtensionProperties";
-
+const AUTHENTICATION_API_VERSION = "1.54.0";
 
 export default function isAuthenticationApiSupported(): boolean {
-    const AUTHENTICATION_API_VERSION = "1.54.0";
-    return semver.gte(
-      tabnineExtensionProperties.vscodeVersion,
-      AUTHENTICATION_API_VERSION
-    );
-  }
+  return semver.gte(
+    tabnineExtensionProperties.vscodeVersion,
+    AUTHENTICATION_API_VERSION
+  );
+}
