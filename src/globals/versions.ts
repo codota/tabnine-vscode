92133ab5a2be70c610b0d3a33c41dba9a955c82e
Dima Abramovich <60742964+dimacodota@users.noreply.github.com>
2021-12-07 11:31:04 +0200

    fix: check minimal authentication provider api version (#716)
    * fix: check minimal authentication provider api version
    
    * prettier
    
    * lint

---
+++ src/globals/versions.ts
@@ -0,0 +1,11 @@
+import * as semver from "semver";
+import tabnineExtensionProperties from "./tabnineExtensionProperties";
+
+const AUTHENTICATION_API_VERSION = "1.54.0";
+
+export default function isAuthenticationApiSupported(): boolean {
+  return semver.gte(
+    tabnineExtensionProperties.vscodeVersion,
+    AUTHENTICATION_API_VERSION
+  );
+}
