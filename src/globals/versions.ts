b15881584cf907c845a77c255be9336e932ec587
Dima <dima@codota.com>
2021-12-07 11:18:33 +0200

    lint

--- src/globals/versions.ts
+++ src/globals/versions.ts
@@ -1,5 +1,6 @@
 import * as semver from "semver";
 import tabnineExtensionProperties from "./tabnineExtensionProperties";
+
 const AUTHENTICATION_API_VERSION = "1.54.0";
 
 export default function isAuthenticationApiSupported(): boolean {
