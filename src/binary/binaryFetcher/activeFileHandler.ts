2370c40d4f2dfc32ce05c1e639623f795b62b76a
yonip23 <yoni@tabnine.com>
2022-01-11 16:36:22 +0200

    patch version 4.0.47

--- src/binary/binaryFetcher/activeFileHandler.ts
+++ src/binary/binaryFetcher/activeFileHandler.ts
@@ -1,11 +1,16 @@
 import * as fs from "fs";
 import { getActivePath, versionPath } from "../paths";
+import { BAD_VERSION } from "./binaryAssistant";
 
 export default function handleActiveFile(): string | null {
   try {
     const activePath = getActivePath();
     if (fs.existsSync(activePath)) {
       const activeVersion = fs.readFileSync(activePath, "utf-8").trim();
+      if (activeVersion === BAD_VERSION) {
+        return null;
+      }
+
       const activeVersionPath = versionPath(activeVersion);
       if (fs.existsSync(activeVersionPath)) {
         return activeVersionPath;
