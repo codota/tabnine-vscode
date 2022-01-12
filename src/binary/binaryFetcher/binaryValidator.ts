ef6ab14134672a47e47d7bfc5374ba4532cff622
yonip23 <67855609+yonip23@users.noreply.github.com>
2022-01-12 10:04:50 +0200

    patch version 4.0.47 (#742)
    * patch version 4.0.47
    
    * rename
    
    Former-commit-id: 3b05f8da858517fdaff0e1b9b31a6d455a8efe25 [formerly dbeb39086601cca1e7461456d17ad14a5de7a1d6] [formerly ff857ef29718266df1f7622967e981b3915a8c48 [formerly 8b6f74c7742931349e3760d0134e99b2c8833d6d]]
    Former-commit-id: da1f23ea7575652f6ec36ccf0d50f1e12e510072 [formerly 3940848aea0b97c8243759619c635808a9a36a42]
    Former-commit-id: 9420759224c2367e866150cb7cd3cc2dd2d86f22

--- src/binary/binaryFetcher/binaryValidator.ts
+++ src/binary/binaryFetcher/binaryValidator.ts
@@ -2,9 +2,12 @@ import * as semver from "semver";
 import { asyncExists } from "../../utils/file.utils";
 import { runProcess } from "../runProcess";
 
+// A patch to skip this specific version, because of a critical issue in the version
+export const BAD_VERSION = "4.0.47";
 const TWO_SECONDS_TIMEOUT = 2000;
+
 export default async function isValidBinary(version: string): Promise<boolean> {
-  if (!(await asyncExists(version))) {
+  if (version === BAD_VERSION || !(await asyncExists(version))) {
     return false;
   }
   const { proc, readLine } = runProcess(version, ["--print-version"]);
