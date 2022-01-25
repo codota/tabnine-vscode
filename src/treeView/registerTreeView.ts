0ac357ea19d7e55efda9fb71eba32586e75b6835
Ofek Ben-Yaish <ofekbenyaish@tabnine.com>
2022-01-25 19:50:26 +0100

    Change tree view to only open home of hub or tabnine app

--- src/treeView/registerTreeView.ts
+++ src/treeView/registerTreeView.ts
@@ -1,6 +1,10 @@
-import { commands, ExtensionContext, window } from "vscode";
-import { getHubStructure } from "../binary/requests/hubStructure";
-import { TABNINE_TREE_NAVIGATION_COMMAND } from "../globals/consts";
+import { commands, ExtensionContext, window, env, Uri } from "vscode";
+import { fireEvent } from "../binary/requests/requests";
+import {
+  TABNINE_APP_URL,
+  TABNINE_OPEN_APP_COMMAND,
+  TABNINE_TREE_NAVIGATION_COMMAND,
+} from "../globals/consts";
 import navigate from "./navigate";
 import TabnineTreeProvider from "./TabnineTreeProvider";
 
@@ -8,24 +12,29 @@ export default async function registerTreeView(
   context: ExtensionContext
 ): Promise<void> {
   try {
-    const structure = await getHubStructure();
-
-    if (structure) {
-      void commands.executeCommand(
-        "setContext",
-        "tabnine.tabnine-navigation-ready",
-        true
-      );
-      context.subscriptions.push(
-        window.registerTreeDataProvider(
-          "tabnine-home",
-          new TabnineTreeProvider()
-        ),
-        commands.registerCommand(TABNINE_TREE_NAVIGATION_COMMAND, (view) => {
-          void navigate(view);
-        })
-      );
-    }
+    context.subscriptions.push(
+      window.registerTreeDataProvider(
+        "tabnine-home",
+        new TabnineTreeProvider()
+      ),
+      commands.registerCommand(TABNINE_TREE_NAVIGATION_COMMAND, (view) => {
+        void navigate(view);
+        void fireEvent({
+          name: "hub-opened-from-sidebar",
+        });
+      }),
+      commands.registerCommand(TABNINE_OPEN_APP_COMMAND, () => {
+        env.openExternal(Uri.parse(TABNINE_APP_URL));
+        void fireEvent({
+          name: "tabnine-app-opened-from-sidebar",
+        });
+      })
+    );
+    void commands.executeCommand(
+      "setContext",
+      "tabnine.tabnine-navigation-ready",
+      true
+    );
   } catch (e) {
     console.error("Error in registerTreeView", e);
   }
