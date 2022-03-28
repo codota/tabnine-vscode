6dfc07199b22125f2e7167f5f9d2667767708831
Dima <dima@codota.com>
2022-03-28 17:54:06 +0300

    open the hub first

--- src/cloudEnvs/authentication.api.ts
+++ src/cloudEnvs/authentication.api.ts
@@ -1,24 +1,9 @@
-import { env, Uri } from "vscode";
-import { configuration } from "../binary/requests/requests";
-import { StateType } from "../globals/consts";
+import navigate from "../treeView/navigate";
 
 export async function openLogin(): Promise<void> {
-  return openBinaryConfig("sign_in");
+  return navigate("sign_in");
 }
 
 export async function openLogout(): Promise<void> {
-  return openBinaryConfig("sign_out");
-}
-
-async function openBinaryConfig(url: string): Promise<void> {
-  const config = await configuration({ quiet: true, source: StateType.AUTH });
-  if (config && config.message) {
-    const localUri = await env.asExternalUri(Uri.parse(config.message));
-
-    await env.openExternal(
-      localUri.with({
-        path: `${localUri.path}/${url}`,
-      })
-    );
-  }
+  return navigate("sign_out");
 }
