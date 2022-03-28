a79cfce38ec1237ccfce486d0c2265fae7ae6387
Dima <dima@codota.com>
2022-03-28 18:04:22 +0300

    use openHub

--- src/cloudEnvs/authentication.api.ts
+++ src/cloudEnvs/authentication.api.ts
@@ -1,9 +1,25 @@
-import navigate from "../treeView/navigate";
+import { env, Uri } from "vscode";
+import { configuration } from "../binary/requests/requests";
+import { StateType } from "../globals/consts";
+import openHub from "../hub/openHub";
 
 export async function openLogin(): Promise<void> {
-  return navigate("sign_in");
+  return openBinaryConfig("sign_in");
 }
 
 export async function openLogout(): Promise<void> {
-  return navigate("sign_out");
+  return openBinaryConfig("sign_out");
+}
+
+async function openBinaryConfig(url: string): Promise<void> {
+  const config = await configuration({ quiet: true, source: StateType.AUTH });
+  if (config && config.message) {
+    const localUri = await env.asExternalUri(Uri.parse(config.message));
+
+    void openHub(
+      localUri.with({
+        path: `${localUri.path}/${url}`,
+      })
+    );
+  }
 }
