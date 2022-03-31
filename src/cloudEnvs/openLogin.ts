09b7949b3f1600409274bfdb5a1150f4ea84f2c3
Dima <dima@codota.com>
2022-04-01 00:20:45 +0300

    lint

--- src/cloudEnvs/openLogin.ts
+++ src/cloudEnvs/openLogin.ts
@@ -7,7 +7,7 @@ export default async function openLogin(): Promise<void> {
   const config = await configuration({ quiet: true, source: StateType.AUTH });
   if (config && config.message) {
     const localUri = await env.asExternalUri(Uri.parse(config.message));
-    const callback = `https://app.tabnine.com/auth/sign-in?tabnineUrl=${localUri}&sync=false`;
+    const callback = `https://app.tabnine.com/auth/sign-in?tabnineUrl=${localUri.toString()}&sync=false`;
     await openHub(localUri);
     void env.openExternal(Uri.parse(callback));
   }
