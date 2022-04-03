2e8daa2d49ece6d373378e83e3315c8ba8a716f5
Dima Abramovich <60742964+dimacodota@users.noreply.github.com>
2022-04-03 11:24:34 +0300

    firebase token file sync (#801)
    Former-commit-id: ebcd3bed9b86d98a05c50cf81eb76b12ce6816ab [formerly b154b272dd7e3745b9b48c6baf9e45253d8f2195] [formerly 397f7fb72fafe709abdea764b7dd999169c141a6 [formerly 98b9fad31056e7c9bd2d931cffae2e5a1e18cf0a]]
    Former-commit-id: 1b2b53122191793ca5d5bdeadb932816ae8dd0c4 [formerly 7cc69fb7980652fef166f39c2ee805f02fb77cb6]
    Former-commit-id: 7d52956dea64d3f477b3c957c8b115da4a31e109

---
+++ src/cloudEnvs/openLogin.ts
@@ -0,0 +1,14 @@
+import { env, Uri } from "vscode";
+import { configuration } from "../binary/requests/requests";
+import { StateType } from "../globals/consts";
+import openHub from "../hub/openHub";
+
+export default async function openLogin(): Promise<void> {
+  const config = await configuration({ quiet: true, source: StateType.AUTH });
+  if (config && config.message) {
+    const localUri = await env.asExternalUri(Uri.parse(config.message));
+    const callback = `https://app.tabnine.com/auth/sign-in?tabnineUrl=${localUri.toString()}&sync=false`;
+    await openHub(localUri);
+    void env.openExternal(Uri.parse(callback));
+  }
+}
