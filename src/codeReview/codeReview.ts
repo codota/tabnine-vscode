ccdb5bdd8079375669a44c456c7cffce50330790
amircodota <25413470+amircodota@users.noreply.github.com>
2022-04-26 10:03:54 +0300

    Feature/code review (#817)
    * detect when a document is opened for diff
    
    * fix how we decide a diff editor is open, and show some dummy comment
    
    * make addComments async + prettier
    
    * support returning multiple threads when commenting
    
    * invoke real api to get comments
    
    * use diff to query for changes only on changed file
    
    * support removing a code review comment
    
    * lint + prettier
    
    * Add status bar message when fetching suggestions
    
    * prettier
    
    * downgrade axios to fix build
    
    * fix tabnine icon in comments
    
    * cleanup
    
    * display formatted code in suggestions
    
    * lint + prettier
    
    * support applying a suggestion
    
    * cleanup
    
    * fire events
    
    * not needed
    
    * do not sent code snippets
    
    * do not do anythong if CODE_REVIEW capability is not enabled
    
    * lint + prettier
    
    * pr comments
    
    * remove unneeded
    
    * lint + prettier
    
    Former-commit-id: dd1d1673c3c96d2ab638fa8cc0a6056c87a4727e [formerly 69b78a1ab9aceb9f9924711b9169abca57997b35] [formerly cf1c2dc651642f35816f41b0dd4380bd2bffb150 [formerly 0f39ccf8f799d53259a0076939846d840ee10267]]
    Former-commit-id: 981e381189704427b12342b0f91322361bbc26ee [formerly 0c76f86b9b22c2d0d3ce080a1ba92ae8b6defd70]
    Former-commit-id: 1db4e5a054e686a3760003aefeb2f8371206cec0

---
+++ src/codeReview/codeReview.ts
@@ -0,0 +1,92 @@
+import * as vscode from "vscode";
+import { Capability, isCapabilityEnabled } from "../capabilities/capabilities";
+import addSuggestions from "./suggestions";
+import DocumentThreads from "./DocumentThreads";
+import TabnineComment from "./TabnineComment";
+
+let activeThreads: DocumentThreads | null = null;
+
+export default function registerCodeReview(): void {
+  const controller = vscode.comments.createCommentController(
+    "tabnine.commentController",
+    ""
+  );
+  controller.options = {
+    placeHolder: "",
+    prompt: "",
+  };
+
+  vscode.commands.registerCommand(
+    "Tabnine.hideSuggestion",
+    (thread: vscode.CommentThread) => {
+      const comment = thread.comments[0] as TabnineComment | undefined;
+
+      if (comment) {
+        comment.hide(thread);
+      }
+    }
+  );
+
+  vscode.commands.registerCommand(
+    "Tabnine.applySuggestion",
+    (thread: vscode.CommentThread) => {
+      const comment = thread.comments[0] as TabnineComment | undefined;
+
+      if (comment && comment.apply(thread)) {
+        thread.dispose();
+      }
+    }
+  );
+
+  vscode.window.onDidChangeActiveTextEditor(async () => {
+    if (!isCapabilityEnabled(Capability.CODE_REVIEW)) return;
+
+    const diffEditor = getActiveDiffEditor();
+
+    let newThreads = null;
+    if (diffEditor) {
+      newThreads = await addSuggestions(
+        controller,
+        diffEditor.newEditor.document,
+        diffEditor.oldEditor.document
+      );
+    }
+
+    if (activeThreads) {
+      activeThreads.dispose();
+    }
+
+    activeThreads = newThreads;
+  });
+}
+
+function getActiveDiffEditor(): {
+  oldEditor: vscode.TextEditor;
+  newEditor: vscode.TextEditor;
+} | null {
+  const visibleEditors = vscode.window.visibleTextEditors;
+  if (visibleEditors.length !== 2) return null;
+  if (
+    visibleEditors[0].document.uri.path !== visibleEditors[1].document.uri.path
+  )
+    return null;
+
+  const oldEditor = visibleEditors.find(isHeadGitEditor);
+  const newEditor = visibleEditors.find(
+    (editor) => editor.document.uri.scheme === "file"
+  );
+
+  if (oldEditor && newEditor) {
+    return { oldEditor, newEditor };
+  }
+  return null;
+}
+
+function isHeadGitEditor(editor: vscode.TextEditor): boolean {
+  if (editor.document.uri.scheme === "git") {
+    const query = JSON.parse(editor.document.uri.query) as { ref: string };
+    return query.ref === "HEAD" || query.ref === "~";
+  }
+
+  return false;
+}
---
+++ src/codeReview/suggestions.ts
@@ -0,0 +1,122 @@
+import {
+  CommentController,
+  CommentThread,
+  CommentThreadCollapsibleState,
+  TextDocument,
+  window,
+  StatusBarAlignment,
+} from "vscode";
+import * as path from "path";
+import * as diff from "diff";
+import * as api from "./api";
+import TabnineComment from "./TabnineComment";
+import { fireEvent } from "../binary/requests/requests";
+import DocumentThreads from "./DocumentThreads";
+
+export default async function addSuggestions(
+  controller: CommentController,
+  document: TextDocument,
+  oldDocument: TextDocument
+): Promise<DocumentThreads | null> {
+  const start = new Date().getTime();
+
+  try {
+    const extensions = await supportedExtensions();
+    if (!extensions.includes(path.extname(document.uri.path))) {
+      return null;
+    }
+
+    const text = document.getText();
+
+    const ranges: api.Range[] = [];
+    const changes = diff.diffLines(text, oldDocument.getText());
+    let currentPosition = 0;
+    changes.forEach((change) => {
+      if (change.removed) {
+        ranges.push({
+          start: currentPosition,
+          end: currentPosition + change.value.length,
+        });
+      }
+
+      if (!change.added) {
+        currentPosition += change.value.length;
+      }
+    });
+
+    const statusBarItem = window.createStatusBarItem(
+      StatusBarAlignment.Left,
+      -1
+    );
+    statusBarItem.text = "Fecthing Tabnine code review suggestions...";
+    statusBarItem.show();
+
+    let response;
+    try {
+      response = await api.querySuggestions({
+        filename: path.basename(document.uri.path),
+        buffer: text,
+        ranges,
+        threshold: "review",
+      });
+    } finally {
+      statusBarItem.dispose();
+    }
+
+    const threads: CommentThread[] = [];
+    response.focus.forEach((focus) => {
+      const suggestion = focus.suggestions.find(
+        (s) => s.classification.type === "other"
+      );
+      if (!suggestion) {
+        return;
+      }
+
+      const line = document.lineAt(document.positionAt(focus.start));
+      const thread = controller.createCommentThread(document.uri, line.range, [
+        new TabnineComment(focus.old_value, suggestion, document.languageId),
+      ]);
+
+      thread.canReply = false;
+      thread.collapsibleState = CommentThreadCollapsibleState.Expanded;
+      thread.label = suggestion.classification.description;
+
+      threads.push(thread);
+    });
+
+    void fireEvent({
+      name: "code-review-diff-task-success",
+      durationMs: new Date().getTime() - start,
+      suggestions: response.focus.length,
+      file: document.uri.path,
+      lineCount: document.lineCount,
+    });
+
+    return new DocumentThreads(document.uri, threads);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    void fireEvent({
+      name: "code-review-diff-task-error",
+      durationMs: new Date().getTime() - start,
+      file: document.uri.path,
+      lineCount: document.lineCount,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      error: e.message || e.toString(),
+    });
+
+    throw e;
+  }
+}
+
+let cachedSupportedExtensions: string[] | null = null;
+async function supportedExtensions(): Promise<string[]> {
+  if (!cachedSupportedExtensions) {
+    cachedSupportedExtensions = (await api.supportedExtensions()).extensions;
+  }
+
+  return cachedSupportedExtensions;
+}
+
+setInterval(() => {
+  cachedSupportedExtensions = null;
+}, 1000 * 60 * 10); // reset extension cache every 10 minutes
