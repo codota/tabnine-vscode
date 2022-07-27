39c1394f7851629b1e087a278090d7319e1cd750
Amir Bilu <amirbilu@gmail.com>
2022-07-27 11:38:10 +0300

    fix tostring

--- src/widgetWebview/WidgetWebviewProvider.ts
+++ src/widgetWebview/WidgetWebviewProvider.ts
@@ -57,7 +57,7 @@ async function setWebviewHtml(
 
       // eslint-disable-next-line no-param-reassign
       webviewView.webview.html = createLayoutTemplate(`
-          <iframe src=${uri} id="active-frame" frameborder="0" sandbox="allow-same-origin allow-pointer-lock allow-scripts allow-downloads allow-forms" allow="clipboard-read; clipboard-write;" style="display: block; margin: 0px; overflow: hidden; position: absolute; width: 100%; height: 100%; visibility: visible;"></iframe>
+          <iframe src=${uri.toString()} id="active-frame" frameborder="0" sandbox="allow-same-origin allow-pointer-lock allow-scripts allow-downloads allow-forms" allow="clipboard-read; clipboard-write;" style="display: block; margin: 0px; overflow: hidden; position: absolute; width: 100%; height: 100%; visibility: visible;"></iframe>
            `);
 
       onWebviewLoaded();
