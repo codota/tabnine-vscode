32d70bf94901aaa9bc4bc0811d33ecb68e5a994d
yoni <yoni@tabnine.com>
2022-09-08 18:15:05 +0300

    revert previous commit
    Former-commit-id: a300eb14dde65630cb42b9eaf478e10e6be0c990 [formerly 55925d726696fa595db4499472633a5aa8fc3f38] [formerly 19cc19bfd5f9a7334bf45d6a8ae3a59328d2c1f0 [formerly 9ffb4fad44b6216b01c122fedf55218eef3f8a2c]]
    Former-commit-id: 4f6bd58201ecb48a89ffa7b2b9718ceb2447cf2e [formerly 26565af2dcff60e11eed92dc2431f15710d72b6c]
    Former-commit-id: 4f104d108a4056408944efb96adeed47975d4ee3

--- src/binary/requests/requests.ts
+++ src/binary/requests/requests.ts
@@ -55,7 +55,6 @@ export type SnippetContext = {
   response_time_ms?: number;
   is_cached?: boolean;
   context_len?: number;
-  first_token_score?: string;
 };
 
 export type AutocompleteResult = {
