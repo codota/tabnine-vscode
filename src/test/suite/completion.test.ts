ae95c8597c20bad4b8fe82e0c7666b358d8779b2
Dima <dima@codota.com>
2022-12-12 18:15:51 +0200

    fix test

--- src/test/suite/completion.test.ts
+++ src/test/suite/completion.test.ts
@@ -134,12 +134,11 @@ describe("Should do completion", () => {
   });
   it("should prefer the popup when only popup is visible and there is no inline suggestion", async () => {
     await openADocWith("cons", "javascript");
-    await emulationUserInteraction();
     await vscode.commands.executeCommand("type", {
       text: `o`,
     });
 
-    await emulationUserInteraction();
+    await sleep(800);
     await triggerSelectionAcceptance();
 
     assertTextIsCommitted("console");
