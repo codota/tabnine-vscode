66b522adb13e15694a191bf3307fd17aaa56b203
Dima <dima@codota.com>
2023-01-01 17:51:17 +0200

    typo

--- src/test/suite/completion.test.ts
+++ src/test/suite/completion.test.ts
@@ -49,10 +49,10 @@ import { SuggestionShownRequestMatcher } from "./utils/SuggestionShownRequestMat
 describe("Should do completion", () => {
   const SPACES_INDENTATION = "    ";
   const TAB_INDENTATION = "\t";
-  const WAIT_LONGER_THAT_DEBOUNCE = 300;
+  const WAIT_LONGER_THAN_SHORT_DEBOUNCE = 300;
   const SHORT_DEBOUNCE_VALUE = 150;
   const LONG_DEBOUNCE_VALUE = 600;
-  const WAIT_LONGER_THAT_LONG_DEBOUNCE = 750;
+  const WAIT_LONGER_THAN_LONG_DEBOUNCE = 750;
 
   beforeEach(() => {
     setupForCompletionsTests();
@@ -329,7 +329,7 @@ describe("Should do completion", () => {
     await vscode.commands.executeCommand("type", {
       text: "d",
     });
-    await sleep(WAIT_LONGER_THAT_DEBOUNCE);
+    await sleep(WAIT_LONGER_THAN_SHORT_DEBOUNCE);
     await acceptInline();
     await emulationUserInteraction();
 
@@ -403,7 +403,7 @@ describe("Should do completion", () => {
     await vscode.commands.executeCommand("type", {
       text: "d",
     });
-    await sleep(WAIT_LONGER_THAT_DEBOUNCE);
+    await sleep(WAIT_LONGER_THAN_SHORT_DEBOUNCE);
 
     verify(
       stdinMock.write(new SuggestionShownRequestMatcher("data"), "utf8")
@@ -426,7 +426,7 @@ describe("Should do completion", () => {
     await vscode.commands.executeCommand("type", {
       text: "o",
     });
-    await sleep(WAIT_LONGER_THAT_LONG_DEBOUNCE);
+    await sleep(WAIT_LONGER_THAN_LONG_DEBOUNCE);
 
     verify(
       stdinMock.write(new SuggestionShownRequestMatcher("data"), "utf8")
