9046b3a13856bfec5132edce50d220b99b85bc8e
Mishig Davaadorj <dmishig@gmail.com>
2023-02-21 15:46:33 +0100

    rm unneded status

--- src/statusBar/statusBarActionHandler.ts
+++ src/statusBar/statusBarActionHandler.ts
@@ -1,16 +1,11 @@
 import * as vscode from "vscode";
 import {
-  sendStatusBarAction,
   StatusBarStatus,
 } from "../binary/requests/statusBar";
 
 import {
-  MessageActionsEnum,
-  NOTIFICATIONS_OPEN_QUERY_PARAM,
   OPEN_LP_FROM_STATUS_BAR,
-  StateType,
   STATUS_BAR_NOTIFICATION_PERIOD,
-  TABNINE_NOTIFICATIONS_FOCUS_COMMAND,
 } from "../globals/consts";
 import {
   promotionTextIs,
@@ -18,7 +13,6 @@ import {
   setPromotionStatus,
 } from "./statusBar";
 import { sleep } from "../utils/utils";
-import openHub from "../hub/openHub";
 
 let statusBarCommandDisposable: vscode.Disposable;
 
@@ -26,7 +20,6 @@ export default function handleStatus(
   context: vscode.ExtensionContext,
   status: StatusBarStatus
 ): void {
-  registerStatusHandlingCommand(status, context);
 
   if (!promotionTextIs(status.message)) {
     // void setState({
@@ -57,46 +50,6 @@ export default function handleStatus(
 async function asyncRemoveStatusAfterDuration(id: string, duration: number) {
   await sleep(duration);
   resetDefaultStatus(id);
-}
-
-function registerStatusHandlingCommand(
-  message: StatusBarStatus,
-  context: vscode.ExtensionContext
-) {
-  statusBarCommandDisposable?.dispose();
-
-  statusBarCommandDisposable = vscode.commands.registerCommand(
-    OPEN_LP_FROM_STATUS_BAR,
-    () => {
-      executeStatusAction(message);
-      void sendStatusBarAction(
-        message.id,
-        message.message,
-        message.notification_type,
-        message.actions,
-        message.state
-      );
-    }
-  );
-
-  context.subscriptions.push(statusBarCommandDisposable);
-}
-function executeStatusAction(message: StatusBarStatus) {
-  const selectedAction = message.actions;
-
-  if (selectedAction?.includes(MessageActionsEnum.OPEN_HUB)) {
-    void openHub(StateType.STATUS)();
-  }
-
-  if (selectedAction?.includes(MessageActionsEnum.OPEN_NOTIFICATIONS)) {
-    void vscode.commands.executeCommand(TABNINE_NOTIFICATIONS_FOCUS_COMMAND);
-  }
-
-  if (selectedAction?.includes(MessageActionsEnum.OPEN_NOTIFICATIONS_IN_HUB)) {
-    void openHub(StateType.STATUS, `/home?${NOTIFICATIONS_OPEN_QUERY_PARAM}`)();
-  }
-
-  resetDefaultStatus(message.id);
 }
 
 export function disposeStatusBarCommand(): void {
