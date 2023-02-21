e1ac4326113800b932dfe5dfbfea1651de1a2a4e
avner <avner@avner.com>
2023-02-21 16:42:18 +0200

    fixes

--- src/binary/binaryFetcher/existingVersionHandler.ts
+++ src/binary/binaryFetcher/existingVersionHandler.ts
@@ -1,18 +1,18 @@
-import { existsSync, promises as fs } from "fs";
+import { promises as fs } from "fs";
 import * as path from "path";
 import sortBySemver from "../../utils/semver.utils";
 import { asyncFind } from "../../utils/utils";
 import isValidBinary from "./binaryValidator";
 import { getRootPath, versionPath } from "../paths";
 import { setDirectoryFilesAsExecutable } from "../utils";
-
+import { asyncExists } from "../../utils/file.utils";
 export default async function handleExistingVersion(): Promise<string | null> {
   try {
     const versionPaths = await fs.readdir(getRootPath());
     await Promise.all(
       versionPaths
         .map((version) => path.dirname(versionPath(version)))
-        .filter((p) => existsSync(p))
+        .filter(async (p) => asyncExists(p))
         .map(async (p2) => setDirectoryFilesAsExecutable(p2))
     );
     const versions = sortBySemver(versionPaths).map(versionPath);
