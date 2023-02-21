7c1269d8b73f3d3b1b5069c3a9cbe2f2bdfe97c8
Mishig Davaadorj <dmishig@gmail.com>
2023-02-21 15:47:42 +0100

    wip

--- src/binary/requests/statusBar.ts
+++ src/binary/requests/statusBar.ts
@@ -1,7 +1,4 @@
 import { MessageAction } from "../../globals/consts";
-import { tabNineProcess } from "./requests";
-
-type StatusBarAction = Record<string, unknown>;
 
 export type StatusBarStatus = {
   id: string;
@@ -12,18 +9,3 @@ export type StatusBarStatus = {
   duration_seconds?: number;
   state: unknown;
 };
-
-export function getStatus(): null {
-  return null;
-}
-export async function sendStatusBarAction(
-  id: string,
-  selected: string | undefined,
-  notification_type: unknown,
-  actions: MessageAction[],
-  state: unknown
-): Promise<StatusBarAction | null | undefined> {
-  return tabNineProcess.request<StatusBarAction>({
-    StatusBarAction: { id, selected, notification_type, actions, state },
-  });
-}
