e81cde28664612e1b7855980e111247157270b4e
Dima <dima@codota.com>
2023-02-21 14:23:59 +0200

    DEV2-2328 - fixes

--- src/testgen/TestGenCodeLensProvider.ts
+++ src/testgen/TestGenCodeLensProvider.ts
@@ -12,44 +12,42 @@ import TabnineCodeLens from "./TabnineCodeLens";
 
 export default class TestGenCodeLensProvider implements CodeLensProvider {
   // eslint-disable-next-line class-methods-use-this
-  public provideCodeLenses(
+  public async provideCodeLenses(
     document: TextDocument
-  ): CodeLens[] | Thenable<CodeLens[] | undefined> {
+  ): Promise<CodeLens[] | undefined> {
     if (isTestGenEnabled()) {
-      return commands
-        .executeCommand<DocumentSymbol[]>(
-          "vscode.executeDocumentSymbolProvider",
-          document.uri
-        )
-        .then((docSymbols) => {
-          const symbolsToFind = [SymbolKind.Function, SymbolKind.Method];
-          const functionsBlocks = docSymbols?.filter((symbol) =>
-            symbolsToFind.includes(symbol.kind)
-          );
-          const classes = docSymbols?.filter(
-            (symbol) => symbol.kind === SymbolKind.Class
-          );
-          classes?.forEach((classSymbol) => {
-            classSymbol.children
-              .filter((child) => symbolsToFind.includes(child.kind))
-              .forEach((method) => {
-                functionsBlocks?.push(method);
-              });
+      const docSymbols = await commands.executeCommand<DocumentSymbol[]>(
+        "vscode.executeDocumentSymbolProvider",
+        document.uri
+      );
+
+      const symbolsToFind = [SymbolKind.Function, SymbolKind.Method];
+      const functionsBlocks = docSymbols?.filter((symbol) =>
+        symbolsToFind.includes(symbol.kind)
+      );
+      const classes = docSymbols?.filter(
+        (symbol) => symbol.kind === SymbolKind.Class
+      );
+      classes?.forEach((classSymbol) => {
+        classSymbol.children
+          .filter((child) => symbolsToFind.includes(child.kind))
+          .forEach((method) => {
+            functionsBlocks?.push(method);
           });
-          return functionsBlocks?.map(
-            (block) =>
-              new TabnineCodeLens(
-                block.selectionRange,
-                document.getText(block.range),
-                document.fileName,
-                block.selectionRange,
-                document.getText(),
-                document.languageId,
-                block.selectionRange.start,
-                document.isUntitled
-              )
-          );
-        });
+      });
+      return functionsBlocks?.map(
+        (block) =>
+          new TabnineCodeLens(
+            block.selectionRange,
+            document.getText(block.range),
+            document.fileName,
+            block.selectionRange,
+            document.getText(),
+            document.languageId,
+            block.selectionRange.start,
+            document.isUntitled
+          )
+      );
     }
     return [];
   }
