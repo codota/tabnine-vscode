fe2b76bef9637578cb95a15d98ac26eb4869c7f0
avner <avner@avner.com>
2023-02-22 18:50:08 +0200

    fixing the build script

--- binaries/downloadBinaries.sh
+++ binaries/downloadBinaries.sh
@@ -12,7 +12,7 @@ gsutil -m cp -r "gs://latest-onprem-binaries/$VERSION/latest/*" ./binaries
 
 for target in x86_64-pc-windows-gnu x86_64-apple-darwin i686-pc-windows-gnu aarch64-apple-darwin x86_64-unknown-linux-musl
 do
-    cd $SCRIPT_DIR/${target} \
+    cd $SCRIPT_DIR/$VERSION/${target} \
             && unzip TabNine.zip \
             && chmod +x * \
             && rm TabNine.zip \
