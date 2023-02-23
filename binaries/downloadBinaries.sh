058160a41b3f3ac1f7bf3b5e3149ea780afc30cf
Assaf Sapir <assaf@tabnine.com>
2023-02-23 09:23:28 +0200

    fix path

--- binaries/downloadBinaries.sh
+++ binaries/downloadBinaries.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "script dir is $SCRIPT_DIR"
@@ -9,12 +10,11 @@ echo "script dir is $SCRIPT_DIR"
 echo "Downloading version $VERSION"
 
 
-#for target in x86_64-pc-windows-gnu x86_64-apple-darwin i686-pc-windows-gnu aarch64-apple-darwin x86_64-unknown-linux-musl
 for target in x86_64-pc-windows-gnu x86_64-apple-darwin i686-pc-windows-gnu aarch64-apple-darwin x86_64-unknown-linux-musl
 do
     mkdir -p $SCRIPT_DIR/$VERSION/${target} \
     && cd $SCRIPT_DIR/$VERSION/${target} \
-    && gsutil -m cp -r "gs://latest-onprem-binaries/$VERSION/latest/${target}/TabNine.zip" . \
+    && gsutil cp -r "gs://latest-onprem-binaries/$VERSION/artifacts/${target}/TabNine.zip" . \
     && unzip TabNine.zip \
     && chmod +x * \
     && rm TabNine.zip \
