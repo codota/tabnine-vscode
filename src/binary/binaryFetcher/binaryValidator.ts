63a2d9fb40be394302743fe990fd9f4e6e3955ce
Amir Bilu <amirbilu@gmail.com>
2023-03-09 11:58:22 +0200

    bump version (#1055)
    Former-commit-id: e2be911d7c0e10de0a35d3305347bb9e9cf9fa30 [formerly 8f1c0f207ee391584b4885ad1c65129670d1c07e] [formerly cdd624efbf0ee8b6071adbfe0bd170b2d6a0df0e [formerly 77d81fc8cf12917fdf64964c24aab89226dd4848]]
    Former-commit-id: e52f5c3c3be3ed9f258ef79355fbdb378ff3341d [formerly 1dbb2896e64854b2989ab2bc93f5920ce4aa26e7]
    Former-commit-id: 76046c5499922c1e6e29075ae5fa43265d6a164b

--- src/binary/binaryFetcher/binaryValidator.ts
+++ src/binary/binaryFetcher/binaryValidator.ts
@@ -1,4 +1,5 @@
 import * as semver from "semver";
+import { ONPREM } from "../../onPrem";
 import { asyncExists } from "../../utils/file.utils";
 import { runProcess } from "../runProcess";
 
@@ -7,6 +8,8 @@ export const BAD_VERSION = "4.0.47";
 const TWO_SECONDS_TIMEOUT = 2000;
 
 export default async function isValidBinary(version: string): Promise<boolean> {
+  if (ONPREM) return true;
+
   if (version === BAD_VERSION || !(await asyncExists(version))) {
     return false;
   }
