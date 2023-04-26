47783aa1a13af5df3b9ad335c299a2327590fa5a
Mishig Davaadorj <dmishig@gmail.com>
2023-04-26 09:09:58 +0200

    rm gettingstartedopenstate

--- src/handlePluginInstalled.ts
+++ src/handlePluginInstalled.ts
@@ -1,14 +1,11 @@
 import { Disposable } from "vscode";
 import { onPluginInstalledEmitter } from "./events/onPluginInstalledEmitter";
-import { isAlreadyOpenedGettingStarted } from "./state/gettingStartedOpenedState";
 import { ExtensionContext } from "./preRelease/types";
 
 export default function handlePluginInstalled(
   context: ExtensionContext
 ): Disposable {
   return onPluginInstalledEmitter.event(() => {
-    if (isAlreadyOpenedGettingStarted(context)) return;
-    // todo: open webview
     console.log("todo open webview");
   })
 }
