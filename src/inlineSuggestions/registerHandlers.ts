8914029ede0bd84a9c3dac66517b58327d41e252
Jason Stillerman <jason.t.stillerman@gmail.com>
2023-04-25 20:09:46 -0400

    fetches code attribution

--- src/inlineSuggestions/registerHandlers.ts
+++ src/inlineSuggestions/registerHandlers.ts
@@ -2,16 +2,19 @@ import {
   commands,
   Disposable,
   languages,
+  Range,
   TextEditor,
   TextEditorSelectionChangeEvent,
   TextEditorSelectionChangeKind,
   window,
   workspace,
 } from "vscode";
+import fetch from "node-fetch";
 import { Capability, isCapabilityEnabled } from "../capabilities/capabilities";
 import {
   ACCEPT_INLINE_COMMAND,
   ESCAPE_INLINE_COMMAND,
+  ATTRIBUTION_COMMAND,
   NEXT_INLINE_COMMAND,
   PREV_INLINE_COMMAND,
   SNIPPET_COMMAND,
@@ -34,7 +37,7 @@ import {
 } from "../globals/versions";
 import { initTabOverride } from "../lookAheadSuggestion";
 
-export const decorationType = window.createTextEditorDecorationType({});
+// export const decorationType = window.createTextEditorDecorationType({});
 
 function isSnippetAutoTriggerEnabled() {
   return isCapabilityEnabled(Capability.SNIPPET_AUTO_TRIGGER);
@@ -72,7 +75,8 @@ export default async function registerInlineHandlers(
         { pattern: "**" },
         inlineCompletionsProvider
       ),
-      ...initTracker()
+      ...initTracker(),
+      registerAttributionHandler()
     );
     await initTabOverride(subscriptions);
     return subscriptions;
@@ -163,9 +167,52 @@ function registerNextHandler(): Disposable {
   );
 }
 
-function registerEscapeHandler(): Disposable {
+function registerEscapeHandler(): Disposable {  
   return commands.registerTextEditorCommand(`${ESCAPE_INLINE_COMMAND}`, () => {
     void clearInlineSuggestionsState();
+  });
+}
+
+function registerAttributionHandler(): Disposable {
+  console.log("REGISTERING ATTRIBUTION HANDLER");
+  
+  return commands.registerTextEditorCommand(`${ATTRIBUTION_COMMAND}`, ({document, selection}) => {
+    const text = document.getText();
+    const url = "https://stack-dev.dataportraits.org/overlap";
+    const body = { document: text };
+    
+    void fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    }).then(async resp => {
+      const json = await resp.json() as any as {spans: [[number, number]]}      
+      const {spans} = json
+
+      // find the biggest span
+      const [startChar, endChar] = spans.reduce((acc, curr) => {
+        const [accStart, accEnd] = acc;
+        const [currStart, currEnd] = curr;
+        const accLength = accEnd - accStart;
+        const currLength = currEnd - currStart;
+        if (currLength > accLength) {
+          return curr;
+        }
+        return acc;
+      }, [0, 0]);
+
+      const decorations = [{range: new Range(document.positionAt(startChar), document.positionAt(endChar)), hoverMessage: "This code is in the stack!"}]
+
+      const decorationType = window.createTextEditorDecorationType({
+        color: 'red',
+        textDecoration: 'underline',
+      });
+      window.activeTextEditor?.setDecorations(decorationType, decorations);
+
+      setTimeout(() => {
+        window.activeTextEditor?.setDecorations(decorationType, []);
+      }, 10000);
+    })
   });
 }
 
