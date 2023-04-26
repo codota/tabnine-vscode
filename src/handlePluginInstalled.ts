d63d18f565d96d75e54394fd885a4f04401e00a9
Mishig Davaadorj <dmishig@gmail.com>
2023-04-26 09:13:50 +0200

    chore

--- src/handlePluginInstalled.ts
+++ src/handlePluginInstalled.ts
@@ -3,7 +3,7 @@ import { onPluginInstalledEmitter } from "./events/onPluginInstalledEmitter";
 import { ExtensionContext } from "./preRelease/types";
 
 export default function handlePluginInstalled(
-  context: ExtensionContext
+  _context: ExtensionContext
 ): Disposable {
   return onPluginInstalledEmitter.event(() => {
     console.log("todo open webview");
