fe7759a0a1fe5ef63fc1bbd61628f15415f5bc31
Jason Stillerman <jason.t.stillerman@gmail.com>
2023-05-03 14:22:11 -0400

    attribution window size

--- src/inlineSuggestions/highlightStackAttributions.ts
+++ src/inlineSuggestions/highlightStackAttributions.ts
@@ -1,6 +1,9 @@
 import {
   Range,
+  Selection,
+  WorkspaceConfiguration,
   window,
+  workspace,
 } from "vscode";
 
 import fetch from "node-fetch";
@@ -9,9 +12,34 @@ export default async function highlightStackAttributions(): Promise<void> {
   const document = window.activeTextEditor?.document
   if (!document) return;
 
+  type Config = WorkspaceConfiguration & {
+    attributionWindowSize: number;
+  };
+  const config: Config = workspace.getConfiguration("HuggingFaceCode") as Config;
+  const { attributionWindowSize } = config;
+
+  // get cursor postion and offset
+  const cursorPosition = window.activeTextEditor?.selection.active;
+  if (!cursorPosition) return;
+  const cursorOffset = document.offsetAt(cursorPosition);
+
+  const start = Math.max(0, cursorOffset - attributionWindowSize);
+  const end = Math.min(document.getText().length, cursorOffset + attributionWindowSize);
+
+  // Select the start to end span
+  if (!window.activeTextEditor) return;
+  window.activeTextEditor.selection = new Selection(document.positionAt(start), document.positionAt(end));
+  // new Range(document.positionAt(start), document.positionAt(end));
+  
+
   const text = document.getText();
+  const textAroundCursor = text.slice(start, end);
+
   const url = "https://stack-dev.dataportraits.org/overlap";
-  const body = { document: text };
+  const body = { document: textAroundCursor };
+
+  // notify user request has started
+  void window.showInformationMessage("Sending request to stack-dev");
     
   const resp = await fetch(url, {
     method: "POST",
@@ -19,33 +47,33 @@ export default async function highlightStackAttributions(): Promise<void> {
     headers: { "Content-Type": "application/json" },
   });
 
-    
-  const json = await resp.json() as any as {spans: [[number, number]]}      
+  const json = await resp.json() as any as {spans: [number, number][]}      
   const {spans} = json
 
-  // find the biggest span
-  const [startChar, endChar] = spans.reduce((acc, curr) => {
-    const [accStart, accEnd] = acc;
-    const [currStart, currEnd] = curr;
-    const accLength = accEnd - accStart;
-    const currLength = currEnd - currStart;
-    if (currLength > accLength) {
-      return curr;
-    }
-    return acc;
-  }, [0, 0]);
+  if(spans.length === 0) {
+    void window.showInformationMessage("No code found in the stack");
+    return;
+  }
 
-  const decorations = [{range: new Range(document.positionAt(startChar), document.positionAt(endChar)), hoverMessage: "This code is in the stack!"}]
+  void window.showInformationMessage("Highlighting code was found in the stack");
+  
+  // console.log("Sent body", body)
+  // console.log("Got response", json)
+  
+  const decorations = spans.map(([startChar, endChar]) => ({range: new Range(document.positionAt(startChar + start), document.positionAt(endChar + start)), hoverMessage: "This code is in the stack!"}))
+  
+  // console.log("Highlighting", decorations.map(d => [d.range.start, d.range.end]));
 
   const decorationType = window.createTextEditorDecorationType({
     color: 'red',
     textDecoration: 'underline',
     
   });
+
   window.activeTextEditor?.setDecorations(decorationType, decorations);
 
   setTimeout(() => {
     window.activeTextEditor?.setDecorations(decorationType, []);
-  }, 3000);
+  }, 5000);
 }
 
