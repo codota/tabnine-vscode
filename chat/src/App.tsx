7f0dcd7ec1d98a026e44e0d90de9b3be412c95f1
yair <yairco1990@gmail.com>
2023-05-17 01:48:19 +0300

    working webview

--- chat/src/App.tsx
+++ chat/src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Chat } from './components/Chat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(): React.ReactElement {
+  return <Chat />
 }
 
 export default App;
