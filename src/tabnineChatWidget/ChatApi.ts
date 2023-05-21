85f366814c65ea5995dce236166b295dba60a984
yair <yairco1990@gmail.com>
2023-05-22 02:31:26 +0300

    improvement

--- src/tabnineChatWidget/ChatApi.ts
+++ src/tabnineChatWidget/ChatApi.ts
@@ -1,13 +1,23 @@
 import { getState } from "../binary/requests/requests";
 import { chatEventRegistry } from './chatEventRegistry'
 
+type GetJwtResponse = {
+    token: string;
+}
+
 export function initChatApi() {
-    chatEventRegistry.registerEvent('get_jwt', async () => {
+    chatEventRegistry.registerEvent<void, GetJwtResponse>('get_jwt', async () => {
         const state = await getState();
+        if (!state) {
+            throw new Error("state is undefined");
+        }
+        if (!state.access_token) {
+            throw new Error("state has no access token");
+        }
         return {
             command: 'send_jwt',
             payload: {
-                token: state?.access_token
+                token: state.access_token
             }
         }
     });
