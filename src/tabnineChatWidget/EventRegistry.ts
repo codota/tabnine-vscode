9963b73564e052943a0b0e6d7cd8dcbe9e883349
yair <yairco1990@gmail.com>
2023-05-22 20:24:34 +0300

    prettier

--- src/tabnineChatWidget/EventRegistry.ts
+++ src/tabnineChatWidget/EventRegistry.ts
@@ -1,25 +1,30 @@
 type Response<T = unknown> = {
-    command: string;
-    payload?: T;
+  command: string;
+  payload?: T;
 };
-type Handler<Req = unknown, Res = unknown> = (payload: Req) => Promise<Response<Res>>;
+type Handler<Req = unknown, Res = unknown> = (
+  payload: Req
+) => Promise<Response<Res>>;
 
 export class EventRegistry {
-    private events: { [key: string]: Handler<any, any> };
+  private events: { [key: string]: Handler<any, any> };
 
-    constructor() {
-        this.events = {};
-    }
+  constructor() {
+    this.events = {};
+  }
 
-    registerEvent<Req, Res>(event: string, handler: Handler<Req, Res>) {
-        this.events[event] = handler;
-    }
+  registerEvent<Req, Res>(event: string, handler: Handler<Req, Res>) {
+    this.events[event] = handler;
+  }
 
-    async handleEvent<Req, Res>(event: string, payload: Req): Promise<Response<Res>> {
-        const handler = this.events[event] as Handler<Req, Res>;
-        if (handler) {
-            return handler(payload);
-        }
-        throw new Error(`Event: ${event} does not exist in the registry`);
+  async handleEvent<Req, Res>(
+    event: string,
+    payload: Req
+  ): Promise<Response<Res>> {
+    const handler = this.events[event] as Handler<Req, Res>;
+    if (handler) {
+      return handler(payload);
     }
+    throw new Error(`Event: ${event} does not exist in the registry`);
+  }
 }
