b31f00b8f782f027b7e31ec88b4c16a71ef5ad2d
yair <yairco1990@gmail.com>
2023-05-24 14:15:55 +0300

    prettier

--- chat/src/hooks/ExtensionCommunicationProvider.tsx
+++ chat/src/hooks/ExtensionCommunicationProvider.tsx
@@ -1,66 +1,71 @@
-import { useEffect } from 'react';
-import { vscode } from '../utils/vscodeApi';
+import { useEffect } from "react";
+import { vscode } from "../utils/vscodeApi";
 
 interface Request<T> {
-    command: string;
-    data?: T;
+  command: string;
+  data?: T;
 }
 
 interface Response<T> {
-    data: T;
+  data: T;
 }
 
 interface ExtensionMessage<T> {
-    id: string;
-    payload: T;
-};
+  id: string;
+  payload: T;
+}
 
 interface PendingRequest<T> {
-    resolve: (value: T) => void;
-    reject: (reason?: any) => void;
+  resolve: (value: T) => void;
+  reject: (reason?: any) => void;
 }
 
 const pendingRequests = new Map<string, PendingRequest<any>>();
 
 let messageCounter = 1;
 function generateUniqueId(): string {
-    messageCounter++;
-    return messageCounter + "";
+  messageCounter++;
+  return messageCounter + "";
 }
 
-export function sendRequestToExtension<RequestPayloadType, ResponsePayloadType>
-    (request: Request<RequestPayloadType>): Promise<ResponsePayloadType> {
-    const id = generateUniqueId();
+export function sendRequestToExtension<RequestPayloadType, ResponsePayloadType>(
+  request: Request<RequestPayloadType>
+): Promise<ResponsePayloadType> {
+  const id = generateUniqueId();
 
-    vscode.postMessage({ id, ...request });
+  vscode.postMessage({ id, ...request });
 
-    // return a promise that will be resolved once the response is received
-    return new Promise((resolve, reject) => {
-        // store the resolve function to call once the response is received
-        pendingRequests.set(id, { resolve, reject });
-    });
+  // return a promise that will be resolved once the response is received
+  return new Promise((resolve, reject) => {
+    // store the resolve function to call once the response is received
+    pendingRequests.set(id, { resolve, reject });
+  });
 }
 
-export function ExtensionCommunicationProvider({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
-        function handleResponse(event: MessageEvent) {
-            const message: ExtensionMessage<any> = event.data;
+export function ExtensionCommunicationProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    function handleResponse(event: MessageEvent) {
+      const message: ExtensionMessage<any> = event.data;
 
-            // if this is a response to a request, resolve the corresponding Promise
-            if (pendingRequests.has(message.id)) {
-                const pendingRequest = pendingRequests.get(message.id);
-                pendingRequest?.resolve(message.payload);
-                pendingRequests.delete(message.id);
-            }
-        }
+      // if this is a response to a request, resolve the corresponding Promise
+      if (pendingRequests.has(message.id)) {
+        const pendingRequest = pendingRequests.get(message.id);
+        pendingRequest?.resolve(message.payload);
+        pendingRequests.delete(message.id);
+      }
+    }
 
-        // listen for messages from the extension
-        window.addEventListener('message', handleResponse);
+    // listen for messages from the extension
+    window.addEventListener("message", handleResponse);
 
-        return () => {
-            window.removeEventListener('message', handleResponse);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("message", handleResponse);
+    };
+  }, []);
 
-    return <>{children}</>;
+  return <>{children}</>;
 }
