753012b00580130c60ca5830caa410a2aeb9e793
yair <yairco1990@gmail.com>
2023-05-24 14:15:55 +0300

    prettier

--- chat/src/components/ChatBotIsTyping.tsx
+++ chat/src/components/ChatBotIsTyping.tsx
@@ -3,21 +3,27 @@ import { ChatBotMessage } from "./ChatBotMessage";
 import { useChatBotQueryData } from "../hooks/useChatBotQueryData";
 
 type Props = {
-    chatMessages: ChatMessages;
-    onTextChange: () => void;
-    onFinish: (finalBotResponse: string) => void;
+  chatMessages: ChatMessages;
+  onTextChange: () => void;
+  onFinish: (finalBotResponse: string) => void;
 };
 
-export function ChatBotIsTyping({ chatMessages, onFinish, onTextChange }: Props): React.ReactElement | null {
-    const chatBotQueryData = useChatBotQueryData();
-    if (!chatBotQueryData) {
-        return null;
-    }
+export function ChatBotIsTyping({
+  chatMessages,
+  onFinish,
+  onTextChange,
+}: Props): React.ReactElement | null {
+  const chatBotQueryData = useChatBotQueryData();
+  if (!chatBotQueryData) {
+    return null;
+  }
 
-    return <ChatBotMessage
-        chatMessages={chatMessages}
-        chatBotQueryData={chatBotQueryData}
-        onFinish={onFinish}
-        onTextChange={onTextChange}
-    />;
+  return (
+    <ChatBotMessage
+      chatMessages={chatMessages}
+      chatBotQueryData={chatBotQueryData}
+      onFinish={onFinish}
+      onTextChange={onTextChange}
+    />
+  );
 }
--- chat/src/utils/fetchChatResponse.ts
+++ chat/src/utils/fetchChatResponse.ts
@@ -1,69 +1,78 @@
 type Input = {
-    text: string;
-    by: "user" | "chat";
+  text: string;
+  by: "user" | "chat";
 }[];
 type OnData = (text: string) => void;
 type OnDone = () => void;
 type OnError = (text: string) => void;
 
-const URL = 'http://localhost:3010/chat/generate_chat_response';
+const URL = "http://localhost:3010/chat/generate_chat_response";
 const TIMEOUT = 3000;
 
-export function fetchChatResponse(input: Input, onData: OnData, onDone: OnDone, onError: OnError) {
-    const fetchData = async () => {
-        const abortController = new AbortController();
-        const timeoutId = setTimeout(() => {
-            abortController.abort();
-        }, TIMEOUT);
+export function fetchChatResponse(
+  input: Input,
+  onData: OnData,
+  onDone: OnDone,
+  onError: OnError
+) {
+  const fetchData = async () => {
+    const abortController = new AbortController();
+    const timeoutId = setTimeout(() => {
+      abortController.abort();
+    }, TIMEOUT);
 
-        try {
-            const response = await fetch(URL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    input
-                }),
-                signal: abortController.signal
-            });
+    try {
+      const response = await fetch(URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          input,
+        }),
+        signal: abortController.signal,
+      });
 
-            if (!response.body) {
-                throw Error('ReadableStream not yet supported in this browser.');
-            }
+      if (!response.body) {
+        throw Error("ReadableStream not yet supported in this browser.");
+      }
 
-            const decoder = new TextDecoder("utf-8");
-            const reader = response.body.getReader();
-            const process = async ({ done, value }: ReadableStreamReadResult<BufferSource>):
-                Promise<ReadableStreamReadResult<BufferSource>> => {
-                if (done) {
-                    onDone();
-                    reader.cancel();
-                    return { done: true };
-                }
-                if (value) {
-                    try {
-                        let chunk = decoder.decode(value, { stream: true });
-                        onData(chunk);
-                    } catch (e) {
-                        console.error("Invalid JSON:", e);
-                    }
-                }
-                // keep reading
-                return await reader.read().then(process);
-            }
-
-            reader.read().then(process).catch(e => {
-                onError(e.message);
-            });
-
-        } catch (err) {
-            console.error(err);
-            onError("Unable to generate a response");
-        } finally {
-            clearTimeout(timeoutId); // clear timeout if fetch is successful
+      const decoder = new TextDecoder("utf-8");
+      const reader = response.body.getReader();
+      const process = async ({
+        done,
+        value,
+      }: ReadableStreamReadResult<BufferSource>): Promise<
+        ReadableStreamReadResult<BufferSource>
+      > => {
+        if (done) {
+          onDone();
+          reader.cancel();
+          return { done: true };
         }
-    };
+        if (value) {
+          try {
+            let chunk = decoder.decode(value, { stream: true });
+            onData(chunk);
+          } catch (e) {}
+        }
+        // keep reading
+        return await reader.read().then(process);
+      };
 
-    fetchData();
+      reader
+        .read()
+        .then(process)
+        .catch(() => {
+          onError("Network error");
+        });
+    } catch (err) {
+      console.error(err);
+      onError("Unable to generate a response");
+    } finally {
+      clearTimeout(timeoutId); // clear timeout if fetch is successful
+    }
+  };
+
+  fetchData();
 }
