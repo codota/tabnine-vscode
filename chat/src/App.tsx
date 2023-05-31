6908006fcfd23eddf9d86f2eeba3b16be731edb9
yair <yairco1990@gmail.com>
2023-05-31 18:45:47 +0300

    fix conversation update

--- chat/src/App.tsx
+++ chat/src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { ChatViewManager } from "./components/ChatViewManager";
 import { ExtensionCommunicationProvider } from "./hooks/ExtensionCommunicationProvider";
+import { ChatDataStateProvider } from "./hooks/useChatDataState";
 import { ChatStateProvider } from "./hooks/useChatState";
 
 function App(): React.ReactElement {
   return (
     <ExtensionCommunicationProvider>
-      <ChatStateProvider>
-        <ChatViewManager />
-      </ChatStateProvider>
+      <ChatDataStateProvider>
+        <ChatStateProvider>
+          <ChatViewManager />
+        </ChatStateProvider>
+      </ChatDataStateProvider>
     </ExtensionCommunicationProvider>
   );
 }
