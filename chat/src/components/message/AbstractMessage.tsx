1738cddfa014b6c320669c1cd77fa8544daabc44
yair <yairco1990@gmail.com>
2023-06-04 17:11:19 +0300

    use constate to manage the message

--- chat/src/components/message/AbstractMessage.tsx
+++ chat/src/components/message/AbstractMessage.tsx
@@ -1,15 +1,10 @@
 import { MessageContainer } from "./MessageContainer";
 import { MessageContent } from "./MessageContent";
-import { ChatMessageProps } from "../../types/ChatTypes";
 
-type Props = {
-  message: ChatMessageProps;
-};
-
-export function AbstractMessage({ message }: Props): React.ReactElement {
+export function AbstractMessage(): React.ReactElement {
   return (
-    <MessageContainer isBot={message.isBot}>
-      <MessageContent message={message} />
+    <MessageContainer>
+      <MessageContent />
     </MessageContainer>
   );
 }
