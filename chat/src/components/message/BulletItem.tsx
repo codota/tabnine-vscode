9372a28962e10152364f22f4d35f022027729d4c
yair <yairco1990@gmail.com>
2023-06-08 02:54:08 +0300

    ui improvements

--- chat/src/components/message/BulletItem.tsx
+++ chat/src/components/message/BulletItem.tsx
@@ -12,20 +12,22 @@ export function BulletItem({ text }: Props): React.ReactElement {
   return (
     <Wrapper>
       <Bullet />
-      <MessageContentType textSegments={textSegments} />
+      <div>
+        <MessageContentType textSegments={textSegments} />
+      </div>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  padding: 0.4rem 0 0.4rem 0.4rem;
   display: flex;
+  padding: 0.4rem 0 0.4rem 0.4rem;
 `;
 
 const Bullet = styled.div`
   background-color: var(--vscode-editor-foreground);
-  width: 6px;
-  height: 6px;
+  width: 5px;
+  height: 5px;
   border-radius: 50%;
   flex-shrink: 0;
   margin-top: 6px;
--- chat/src/utils/messageFormatter.spec.ts
+++ chat/src/utils/messageFormatter.spec.ts
@@ -1,50 +1,103 @@
 import { getMessageSegments } from "./messageFormatter";
 
 describe("Test getMessageSegments", () => {
-  test("renders learn react link", () => {
-    const result = getMessageSegments(`
-    Some text with 'highlight' and also a **bold** text
-    ${"```"}javascript
-    const a = 1;
-    const b = 2;
-    ${"```"}
-    and also these bullets:
-    - bullet 1
-    - bullet 2
-    and this is the end
-    `);
-    console.log(result);
-  });
-
   test("", () => {
-    const result = getMessageSegments(`Sure, here are some attributes of the SOLID acronym:
+    const result = getMessageSegments(`
 
-- It is an acronym that represents five principles of object-oriented programming and design.
-- It is a mnemonic for five design principles that are intended to make software design more maintainable, flexible, and scalable.
-- The **SOLID** acronym was created by Robert C. Martin, a software developer and author of the book "Object-Oriented Software Construction."
-- The **SOLID** principles were introduced by Michael Feathers, a software developer and author of the book "Agile Software Development: Principles, Patterns, and Practices."
-- The **SOLID** principles are intended to make software design more understandable, flexible, and maintainable by focusing on the following five design principles:
+Hey, **This is a test** that 'checks' the segments 'of a text'.
+- 'bullet' 1
+- **bullet** 2
 
-Here is some code example
 ${"```"}javascript
 const a = 1;
 const b = 2;
 ${"```"}
 
-There are another bullets
+${"```"}
+int a = 1;
+int b = 2;
+${"```"}
 
-- Single Responsibility Principle (SRP): A class should have only one reason to change, meaning that a class should have only one responsibility.
-- Open/Closed Principle (OCP): Software entities should be open for extension but closed for modification. This means that you should be able to extend the behavior of a class without modifying its source code.
-- Liskov Substitution Principle (LSP): Subtypes must be substitutable for their base types. This means that if a parent class can be used where a child class is expected, then an object of the child class can be used in place of an object of the parent class without affecting the correctness of the program.
-- Interface Segregation Principle (ISP): Clients should not be forced to depend on interfaces they do not use. This means that a class should not be required to implement interfaces it does not need.
-- Dependency Inversion Principle (DIP): High-level modules should not depend on low-level modules. Both should depend on abstractions. This means that you should depend on abstractions, not on concrete implementations.
 
-By **following** these 'principles', you 'can create' more maintainable and scalable code that is easier to test and debug.`);
-    console.log(result);
-  });
 
-  test.only("renders learn react link", () => {
-    const result = getMessageSegments(`**bold text** some text`);
-    console.log(result);
+1. bullet **number** 1
+2. bullet 'number' 2
+
+
+`);
+    expect(result).toStrictEqual([
+      {
+        type: "text",
+        content: "\nHey, ",
+      },
+      {
+        type: "bold",
+        content: "This is a test",
+      },
+      {
+        type: "text",
+        content: " that ",
+      },
+      {
+        type: "highlight",
+        content: "checks",
+      },
+      {
+        type: "text",
+        content: " the segments 'of a text'.\n",
+      },
+      {
+        type: "bullet",
+        content: "'bullet' 1",
+      },
+      {
+        type: "text",
+        content: "\n",
+      },
+      {
+        type: "bullet",
+        content: "**bullet** 2",
+      },
+      {
+        type: "text",
+        content: "\n",
+      },
+      {
+        type: "code",
+        content: "const a = 1;\nconst b = 2;",
+        language: "javascript",
+      },
+      {
+        type: "text",
+        content: "\n",
+      },
+      {
+        type: "code",
+        content: "int a = 1;\nint b = 2;",
+        language: "",
+      },
+      {
+        type: "text",
+        content: "\n",
+      },
+      {
+        type: "bulletNumber",
+        content: "bullet **number** 1",
+        number: "1",
+      },
+      {
+        type: "text",
+        content: "\n",
+      },
+      {
+        type: "bulletNumber",
+        content: "bullet 'number' 2",
+        number: "2",
+      },
+      {
+        type: "text",
+        content: "\n",
+      },
+    ]);
   });
 });
