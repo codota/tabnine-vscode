80f006985bed666d8b5870e9759ad23496290f89
Dima Abramovich <60742964+dimacodota@users.noreply.github.com>
2023-06-21 16:48:46 +0300

    DEV2-2966 add diagnostic text as context (#1155)
    * DEV2-2966 add diagnostic text as context
    
    * replace selection on insert
    
    * bump chat version

--- src/tabnineChatWidget/handlers/insertAtCursor.ts
+++ src/tabnineChatWidget/handlers/insertAtCursor.ts
@@ -3,7 +3,13 @@ import * as vscode from "vscode";
 export function insertTextAtCursor({ code }: { code: string }): void {
   const editor = vscode.window.activeTextEditor;
   if (editor) {
-    const position = editor.selection.active;
-    void editor.insertSnippet(new vscode.SnippetString(code), position);
+    if (!editor.selection.isEmpty) {
+      void editor.edit((editBuilder) => {
+        editBuilder.replace(editor.selection, code);
+      });
+    } else {
+      const position = editor.selection.active;
+      void editor.insertSnippet(new vscode.SnippetString(code), position);
+    }
   }
 }
