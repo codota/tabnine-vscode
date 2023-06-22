4fbf82693bee4c72d55eb02f1b2e5d56baad20d1
Dima <dima@codota.com>
2023-06-22 15:58:28 +0300

    DEV2-2966 insert as refactoring action

--- src/tabnineChatWidget/handlers/insertAtCursor.ts
+++ src/tabnineChatWidget/handlers/insertAtCursor.ts
@@ -3,10 +3,13 @@ import * as vscode from "vscode";
 export function insertTextAtCursor({ code }: { code: string }): void {
   const editor = vscode.window.activeTextEditor;
   if (editor) {
+    const edit = new vscode.WorkspaceEdit();
+    edit.replace(editor.document.uri, editor.selection, code, {
+      needsConfirmation: true,
+      label: "Insert code",
+    });
     if (!editor.selection.isEmpty) {
-      void editor.edit((editBuilder) => {
-        editBuilder.replace(editor.selection, code);
-      });
+      void vscode.workspace.applyEdit(edit);
     } else {
       const position = editor.selection.active;
       void editor.insertSnippet(new vscode.SnippetString(code), position);
