cc47915a9b4c2365a73d0f6929580863c34276c7
Dima Abramovich <60742964+dimacodota@users.noreply.github.com>
2023-06-22 16:34:59 +0300

    DEV2-2966 insert as refactoring action (#1158)
    * DEV2-2966 insert as refactoring action
    
    * fix

--- src/tabnineChatWidget/handlers/insertAtCursor.ts
+++ src/tabnineChatWidget/handlers/insertAtCursor.ts
@@ -4,9 +4,12 @@ export function insertTextAtCursor({ code }: { code: string }): void {
   const editor = vscode.window.activeTextEditor;
   if (editor) {
     if (!editor.selection.isEmpty) {
-      void editor.edit((editBuilder) => {
-        editBuilder.replace(editor.selection, code);
+      const edit = new vscode.WorkspaceEdit();
+      edit.replace(editor.document.uri, editor.selection, code, {
+        needsConfirmation: true,
+        label: "Insert code",
       });
+      void vscode.workspace.applyEdit(edit);
     } else {
       const position = editor.selection.active;
       void editor.insertSnippet(new vscode.SnippetString(code), position);
