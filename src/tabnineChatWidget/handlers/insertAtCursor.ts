95bf5263cc97ab4457180c15950bc6947639dca0
Dima <dima@codota.com>
2023-06-22 16:01:17 +0300

    fix

--- src/tabnineChatWidget/handlers/insertAtCursor.ts
+++ src/tabnineChatWidget/handlers/insertAtCursor.ts
@@ -3,12 +3,12 @@ import * as vscode from "vscode";
 export function insertTextAtCursor({ code }: { code: string }): void {
   const editor = vscode.window.activeTextEditor;
   if (editor) {
-    const edit = new vscode.WorkspaceEdit();
-    edit.replace(editor.document.uri, editor.selection, code, {
-      needsConfirmation: true,
-      label: "Insert code",
-    });
     if (!editor.selection.isEmpty) {
+      const edit = new vscode.WorkspaceEdit();
+      edit.replace(editor.document.uri, editor.selection, code, {
+        needsConfirmation: true,
+        label: "Insert code",
+      });
       void vscode.workspace.applyEdit(edit);
     } else {
       const position = editor.selection.active;
