f62bda306957e992e2ddda348c09488110e3696b
Dima Abramovich <60742964+dimacodota@users.noreply.github.com>
2023-06-28 15:50:21 +0300

    DEV2-3023 fix selection (#1172)
    Former-commit-id: 04e372f436522cf72e84374f47a12d79304da7ba [formerly bd8874f4001abec8d9d1c43902234d8eedd45e09] [formerly 92ae96027479d6f971bfe91823cdfb921a76d89a [formerly ac2c9ef1514174bc6d33c926f50dd35771760be9]]
    Former-commit-id: bb84c04717a58a47a90d7d6ef585602092058a7c [formerly ee82f390addb870a6d970b40a885530e36806821]
    Former-commit-id: a2cb1a4618b5fae7abedca32ba288c3760f65a07

--- src/tabnineChatWidget/handlers/insertAtCursor.ts
+++ src/tabnineChatWidget/handlers/insertAtCursor.ts
@@ -4,12 +4,9 @@ export function insertTextAtCursor({ code }: { code: string }): void {
   const editor = vscode.window.activeTextEditor;
   if (editor) {
     if (!editor.selection.isEmpty) {
-      const edit = new vscode.WorkspaceEdit();
-      edit.replace(editor.document.uri, editor.selection, code, {
-        needsConfirmation: true,
-        label: "Insert code",
+      void editor.edit((editBuilder) => {
+        editBuilder.replace(editor.selection, code);
       });
-      void vscode.workspace.applyEdit(edit);
     } else {
       const position = editor.selection.active;
       void editor.insertSnippet(new vscode.SnippetString(code), position);
