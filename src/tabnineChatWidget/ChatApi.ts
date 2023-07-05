2d18defa8b72dc80e6374ad83b2fe2860326aad0
Dima <dima@codota.com>
2023-07-06 00:24:58 +0300

    DEV2-2899 sypport peek definition

--- src/tabnineChatWidget/ChatApi.ts
+++ src/tabnineChatWidget/ChatApi.ts
@@ -9,7 +9,8 @@ import {
 } from "./handlers/getEditorContextHandler";
 import { insertTextAtCursor } from "./handlers/insertAtCursor";
 import { Capability, isCapabilityEnabled } from "../capabilities/capabilities";
-import { lineClicked } from "./handlers/lineclicked";
+import { resolveSymbols } from "./handlers/resolveSymbols";
+import { peekDefinition } from "./handlers/peekDefinition";
 
 type GetUserResponse = {
   token: string;
@@ -95,10 +96,15 @@ export function initChatApi(context: vscode.ExtensionContext) {
     "insert-at-cursor",
     insertTextAtCursor
   );
-  chatEventRegistry.registerEvent<{ code: string; lineNumber: number }, void>(
-    "line-clicked",
-    lineClicked
-  );
+  chatEventRegistry.registerEvent<
+    { symbol: string },
+    vscode.SymbolInformation[] | undefined
+  >("resolve-symbols", resolveSymbols);
+
+  chatEventRegistry.registerEvent<
+    { symbols: vscode.SymbolInformation[] },
+    void
+  >("peek-definition", peekDefinition);
 
   chatEventRegistry.registerEvent<ChatConversation, void>(
     "update_chat_conversation",
