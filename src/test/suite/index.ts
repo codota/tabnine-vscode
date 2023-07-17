db08ce7f05b090cff11f44377ff61c5dc03de20d
Amir Bilu <amirbilu@gmail.com>
2023-07-17 13:17:57 +0300

    dep update

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -4,7 +4,7 @@ import * as glob from "glob";
 
 // This is required to run the tests. Do not change to default export.
 // eslint-disable-next-line import/prefer-default-export
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: "tdd",
@@ -14,28 +14,19 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, "..");
 
-  return new Promise((c, e) => {
-    glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        e(err);
-      }
+  try {
+    const files = await glob.glob("**/**.test.js", { cwd: testsRoot });
 
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+    // Add files to the test suite
+    files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (error) {
-        console.error(error);
-        e(error);
+    // Run the mocha test
+    mocha.run((failures) => {
+      if (failures > 0) {
+        throw new Error(`${failures} tests failed.`);
       }
     });
-  });
+  } catch (error) {
+    console.error(error);
+  }
 }
