dc2da3703bac91c5ffbe5398dbcc7df04cab0384
yonip23 <yoni@tabnine.com>
2023-08-09 22:15:54 +0300

    DEV2-3340 search in both snake case and camel case

---
+++ src/utils/string.utils.ts
@@ -0,0 +1,7 @@
+export function toSnakeCase(str: string): string {
+  return str.replace(/(?!^)[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+}
+
+export function toCamelCase(str: string): string {
+  return str.replace(/_(.)/g, (_match, group1: string) => group1.toUpperCase());
+}
