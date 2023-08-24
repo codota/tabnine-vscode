dad1c044d2cb88929842443b050714b2ff0cbba0
Assaf Sapir <assaf@tabnine.com>
2023-08-24 18:06:40 +0300

    wait also on retry

--- src/enterprise/extension.ts
+++ src/enterprise/extension.ts
@@ -59,6 +59,7 @@ export async function activate(
           void isHealthyServer().then((isHealthy) => {
             if (isHealthy) {
               tryToUpdate();
+              statusBar.waitForProcess();
             }
           });
         }
