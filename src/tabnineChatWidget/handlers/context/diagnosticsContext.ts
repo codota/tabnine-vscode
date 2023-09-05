09df11095c088cb837c9defebe36d4abf042f373
yair <yairco1990@gmail.com>
2023-09-05 20:47:59 +0300

    expand diagnostics and selected_code contexts

--- src/tabnineChatWidget/handlers/context/diagnosticsContext.ts
+++ src/tabnineChatWidget/handlers/context/diagnosticsContext.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { ContextTypeData, DiagnosticsContext } from "./enrichingContextTypes";
+import { ContextTypeData, DiagnosticItem } from "./enrichingContextTypes";
 
 export default async function getDiagnosticsContext(
   editor: vscode.TextEditor
@@ -7,23 +7,32 @@ export default async function getDiagnosticsContext(
   const diagnosticsContext = buildDiagnosticsContext(editor);
   if (!diagnosticsContext) return undefined;
 
-  return Promise.resolve({ type: "Diagnostics", ...diagnosticsContext });
+  return Promise.resolve({
+    type: "Diagnostics",
+    diagnostics: diagnosticsContext,
+  });
 }
 
 function buildDiagnosticsContext(
   editor: vscode.TextEditor
-): DiagnosticsContext | undefined {
-  const visibleDiagnostics = vscode.languages
-    .getDiagnostics(editor.document.uri)
-    .filter(
-      (e) =>
-        e.severity === vscode.DiagnosticSeverity.Error &&
-        editor.visibleRanges.some((r) => r.contains(e.range))
-    );
-  if (!visibleDiagnostics.length) return undefined;
-  return { diagnosticsText: formatDiagnostics(visibleDiagnostics) };
-}
+): DiagnosticItem[] {
+  const selectedRange = editor.selection;
 
-function formatDiagnostics(diagnostics: vscode.Diagnostic[]): string {
-  return `${diagnostics.map((e) => `${e.message}`).join("\n")}`;
+  return vscode.languages
+    .getDiagnostics(editor.document.uri)
+    .filter((e: vscode.Diagnostic) => {
+      if (e.severity !== vscode.DiagnosticSeverity.Error) {
+        return false;
+      }
+      if (selectedRange.isEmpty) {
+        return editor.visibleRanges.some((r) => r.contains(e.range));
+      } else {
+        return selectedRange.contains(e.range);
+      }
+    })
+    .map((e: vscode.Diagnostic) => ({
+      errorMessage: e.message,
+      lineCode: editor.document.lineAt(e.range.start.line).text.trim(),
+      lineNumber: e.range.start.line + 1,
+    }));
 }
