deefd09cbea6bbc6891867e7f4d7c38255237f10
amircodota <25413470+amircodota@users.noreply.github.com>
2023-10-19 17:12:18 +0300

    DEV2-4029 Always go to the binary to get chat url (#1332)
    * DEV2-4029 Always go to the binary to get chat url
    
    * lint
    
    * prettier

--- src/tabnineChatWidget/ChatApi.ts
+++ src/tabnineChatWidget/ChatApi.ts
@@ -187,17 +187,8 @@ export function initChatApi(
     )
     .registerEvent<ServerUrlRequest, ServerUrl>(
       "get_server_url",
-      async (request) => {
-        const isOnPrem = !!serverUrl;
-        if (isOnPrem || isCapabilityEnabled(Capability.CHAT_URL_FROM_BINARY)) {
-          return {
-            serverUrl: await getChatCommunicatorAddress(request.kind),
-          };
-        }
-
-        return {
-          serverUrl: serverUrl ?? "https://api.tabnine.com",
-        };
-      }
+      async (request) => ({
+        serverUrl: await getChatCommunicatorAddress(request.kind),
+      })
     );
 }
