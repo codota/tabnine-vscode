4f4dad61c1a5571801ce457714db46d2bc127bc6
Yoni Davidson <yonidavidson@tabnine.com>
2023-11-01 15:53:58 +0200

    use lanuages from vscode

--- src/tabnineChatWidget/handlers/context/basicContextHandler.ts
+++ src/tabnineChatWidget/handlers/context/basicContextHandler.ts
@@ -16,7 +16,7 @@ export async function getBasicContext(): Promise<BasicContext> {
   const metadata = await getFileMetadata(editor.document.fileName);
   return {
     fileUri: editor.document.uri.toString(),
-    language: editor.document.languageId ,
+    language: editor.document.languageId || await getPredominantWorkspaceLanguage() ,
     metadata,
   };
 }
@@ -47,53 +47,61 @@ async function noEditorResponse() {
 async function getPredominantWorkspaceLanguage(maxFiles: number = 50): Promise<string | undefined> {
   const breakdown: { [extension: string]: number } = {};
 
-    // File extension to language name mapping as a Map
-    const fileTypeToLanguage = new Map<string, string>([
-        ['js', 'javascript'],
-        ['py', 'python'],
-        ['java', 'java'],
-        ['cpp', 'c++'],
-        ['hpp', 'c++'],
-        ['c', 'c'],
-        ['h', 'c'],
-        ['cs', 'c#'],
-        ['php', 'php'],
-        ['ts', 'typescript'],
-        ['rb', 'ruby'],
-        ['go', 'go'],
-        ['rs', 'rust'],
-        ['swift', 'swift'],
-        ['kt', 'kotlin'],
-        ['dart', 'dart'],
-        ['json', 'json'],
-        ['yaml', 'yaml'],
-        ['yml', 'yaml']
-    ]);
+  // Getting the known languages from VS Code
+  const knownLanguages = await vscode.languages.getLanguages();
 
-    const topFileTypes = Array.from(fileTypeToLanguage.keys());
+  // File extension to VS Code language ID mapping
+  const fileTypeToLanguageID = new Map<string, string>([
+      ['js', 'javascript'],
+      ['py', 'python'],
+      ['java', 'java'],
+      ['cpp', 'cpp'],
+      ['hpp', 'cpp'],
+      ['c', 'c'],
+      ['h', 'c'],
+      ['cs', 'csharp'],
+      ['php', 'php'],
+      ['ts', 'typescript'],
+      ['rb', 'ruby'],
+      ['go', 'go'],
+      ['rs', 'rust'],
+      ['swift', 'swift'],
+      ['kt', 'kotlin'],
+      ['dart', 'dart'],
+      ['json', 'json'],
+      ['yaml', 'yaml'],
+      ['yml', 'yaml']
+  ]);
 
-    // Ensure a workspace is open
-    if (!vscode.workspace.workspaceFolders) {
-        console.log("No workspace is open.");
-        return;
-    }
+  const topFileTypes = Array.from(fileTypeToLanguageID.keys());
 
-    // Get up to maxFiles from the workspace without depth restriction
-    const files = await vscode.workspace.findFiles('**/*', null, maxFiles);
+  // Ensure a workspace is open
+  if (!vscode.workspace.workspaceFolders) {
+      console.log("No workspace is open.");
+      return;
+  }
 
-    // Tally up file extensions
-    for (const file of files) {
-        const ext = file.fsPath.split('.').pop()?.toLowerCase();
-        if (ext && topFileTypes.includes(ext)) {
-            if (!breakdown[ext]) {
-                breakdown[ext] = 0;
-            }
-            breakdown[ext]++;
-        }
-    }
+  // Get up to maxFiles from the workspace without depth restriction
+  const files = await vscode.workspace.findFiles('**/*', null, maxFiles);
 
-    // Sort extensions by frequency and return the name of the most common language
-    const sortedExtensions = Object.entries(breakdown).sort(([, a], [, b]) => b - a);
-    const predominantFileType = sortedExtensions[0][0];
-    return sortedExtensions.length > 0 ? fileTypeToLanguage.get(predominantFileType) : undefined;
+  // Tally up file extensions
+  for (const file of files) {
+      const ext = file.fsPath.split('.').pop()?.toLowerCase();
+      if (ext && topFileTypes.includes(ext)) {
+          if (!breakdown[ext]) {
+              breakdown[ext] = 0;
+          }
+          breakdown[ext]++;
+      }
+  }
+
+  // Sort extensions by frequency and return the predominant language ID
+  const sortedExtensions = Object.entries(breakdown).sort(([, a], [, b]) => b - a);
+  const predominantFileType = sortedExtensions[0][0];
+  const languageID = fileTypeToLanguageID.get(predominantFileType);
+  if (!languageID) {
+      return;
+  }
+  // Check if the languageID is among known languages
+  return sortedExtensions.length > 0 && knownLanguages.includes(languageID) ? languageID : undefined;
 }
