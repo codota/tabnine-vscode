1048e38e1d88e4e2b43b383b96535446be15daaf
Amir Tuval <amirtu@tabnine.com>
2023-11-27 22:26:06 +0200

    DEV2-4344 add workspace trace

--- src/WorkspaceUpdater.ts
+++ src/WorkspaceUpdater.ts
@@ -2,7 +2,8 @@ import * as vscode from "vscode";
 import { BINARY_UPDATE_WORKSPACE_INTERVAL } from "./globals/consts";
 import sendUpdateWorkspaceRequest from "./binary/requests/workspace";
 import { Logger } from "./utils/logger";
-import { tabNineProcess } from "./binary/requests/requests";
+import { fireEvent, tabNineProcess } from "./binary/requests/requests";
+import { Capability, isCapabilityEnabled } from "./capabilities/capabilities";
 
 const INITIAL_DELAY = 3000;
 
@@ -28,11 +29,30 @@ export class WorkspaceUpdater implements vscode.Disposable {
   }
 }
 
+async function sendEvent(event: string, payload: Record<string, unknown> = {}) {
+  if (isCapabilityEnabled(Capability.WORKSPACE_TRACE)) {
+    fireEvent({
+      name: event,
+      ...payload,
+    });
+  }
+}
+
 function updateWorkspace() {
+  void sendEvent("workspace_starting");
   const rootPaths = vscode.workspace.workspaceFolders
-    ?.filter((wf) => wf.uri.scheme === "file")
+    ?.filter((wf) => {
+      const isFileUrl = wf.uri.scheme === "file";
+      if (!isFileUrl) {
+        void sendEvent("workspace_protocol_not_file", {
+          path: wf.uri.toString(),
+        });
+      }
+      return isFileUrl;
+    })
     .map((wf) => wf.uri.path);
   if (!rootPaths) {
+    void sendEvent("workspace_no_root_paths");
     Logger.debug(
       `No root paths for project ${vscode.workspace.name || "unknown"}`
     );
@@ -44,6 +64,11 @@ function updateWorkspace() {
       vscode.workspace.name || "unknown"
     }: ${JSON.stringify(rootPaths)}`
   );
+
+  void sendEvent("workspace_sending_request", {
+    root_paths_len: rootPaths.length,
+  });
+
   void sendUpdateWorkspaceRequest({
     root_paths: rootPaths,
   });
